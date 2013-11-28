4511f44cd3bf4ddce5844fa6758d7156213f00c1
Martin Jambon <martin@mjambon.com>
2013-11-27 17:00:10 -0800

    Missing file

---
+++ js/sched2.js
@@ -0,0 +1,80 @@
+/* Scheduling - step 2 */
+
+var sched2 = (function() {
+  var mod = {};
+
+  function viewOfOption(profs, calOption) {
+    var view = $("<div/>")
+      .attr("id", calOption.label);
+    sched.viewOfSuggestion(calOption.slot)
+      .appendTo(view);
+    return view;
+  }
+
+  function viewOfOptions(profs, task, onSelect) {
+    var view = $("<div/>");
+    var state = sched.getState(task);
+
+    var options = state.calendar_options;
+
+    function showOne(id) {
+      $("#" + id)
+        .addClass("sched-highlight");
+    }
+
+    function hideOne(id) {
+      $("#" + id)
+        .removeClass("sched-highlight");
+    }
+
+    var idList = list.map(options, function(x) { return x.label; });
+    var selector = show.create(idList, showOne, hideOne);
+
+    list.iter(options, function(x) {
+      viewOfOption(profs, x)
+        .click(function() {
+          selector.show(x.label);
+          onSelect(x);
+        })
+        .appendTo(view);
+    });
+
+    return view;
+  }
+
+  function updateTask(profs, task, calOption) {
+    var state = sched.getState(task);
+    task.task_progress = "Confirmed";
+    state.scheduling_stage = "Confirm";
+    state.reserved = {
+      slot: calOption.slot,
+      notifs: []
+    };
+    api.postTask(task)
+      .done(function () { sched.loadStep3(profs, task); });
+  }
+
+  mod.load = function(profs, task, view) {
+    $("<h1>Select a final time.</h1>")
+      .appendTo(view);
+
+    var next = $("<button disabled class='btn btn-default'>Next</button>");
+    var selected;
+
+    function onSelect(x) {
+      selected = x;
+      next.attr("disabled", false);
+    }
+
+    viewOfOptions(profs, task, onSelect)
+      .appendTo(view);
+
+    next
+      .appendTo(view)
+      .click(function() {
+        updateTask(profs, task, selected);
+      });
+  };
+
+  return mod;
+}());
