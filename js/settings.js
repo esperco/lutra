29e27752c779c759fde72fef495b18d2528bdc58
Jeff Meister <jmeister@mylife.com>
2014-02-20 15:30:11 -0800

    Bad merge

--- js/settings.js
+++ js/settings.js
@@ -249,9 +249,13 @@ var settings = (function() {
   function displayExecutives(teams) {
     var view = $("#settings-executives");
     view.children().remove();
-    var uniqUIDs = list.unique(Object.keys(leaders));
+    var leaderUIDs = list.unique(
+      list.map(teams, function(team) {
+        return team.team_leaders[0];
+      })
+    );
     var deferredDeferredRows =
-      profile.mget(uniqUIDs).then(function(execProfs) {
+      profile.mget(leaderUIDs).then(function(execProfs) {
         return list.map(execProfs, function(execProf) {
           var prof = execProf.prof;
           var execUid = prof.profile_uid;
