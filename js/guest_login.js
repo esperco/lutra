cb26600ee55618e5a44f43297cb2823902613607
Haoyang Wang <haoyang@esper.co>
2014-02-26 18:30:06 -0800

    let guest choose one time and place

--- js/guest_login.js
+++ js/guest_login.js
@@ -2,17 +2,23 @@ var login = function () {
   var mod = {};
 
   var uid;
+  var chatid;
 
   mod.load = function(task) {
     list.iter(task.task_chats, function(chat) {
       if (chat.chat_with) {
         uid = chat.chat_with;
+        chatid = chat.chatid;
       }
     });
   }
 
   mod.me = function() {
     return uid;
+  }
+
+  mod.myChatid = function() {
+    return chatid;
   }
 
   return mod;
