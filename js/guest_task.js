c8e1bfcd5328e967d57a37e77051d120f6f014d6
Nicholas Chen <nick@esper.co>
2014-02-26 17:13:13 -0800

    bridge styling

--- js/guest_task.js
+++ js/guest_task.js
@@ -34,10 +34,10 @@ var guestTask = function() {
   function googleCalendarURL(text1, text2, slot) {
     return "http://www.google.com/calendar/event?"
          + ["action=TEMPLATE",
-            "text=" + encodeURIComponent(text2),
+            "text=" + encodeURIComponent(text1),
             "dates=" + stripTimestamp(slot.start)
                + "/" + stripTimestamp(slot.end),
-            "details=" + encodeURIComponent(text1),
+            "details=" + encodeURIComponent("For meeting details, click here: " + text2),
             "location=" + encodeURIComponent(locationText(slot.location)),
             "trp=true",             // show as busy
             "sprop=Esper",          // website name
@@ -52,19 +52,36 @@ var guestTask = function() {
            .append($("<img/>", {src:buttonImg, border:0}));
   }
 
-  function viewOfCalendarSlot(x) {
-    var view = $("<div class='sug-details'/>");
+
+  function googleMapsLink(x) {
+    // var getDirections = $("<div>Get directions</div>");
+
+    // var directionsURL = "http://maps.google.com/?daddr="
+    //   + x.location.coord.lat + "," + x.location.coord.lon;
+
+    // var directions =
+    //   $("<a/>", {
+    //     href: directionsURL,
+    //     "class": ""
+    //   }).appendTo(getDirections);
+
+    // getDirections.click(function() { this.target = "_blank"; });
+
+    // return getDirections;
+  }
+
+  function calendarOptions() {
+  }
+
+  function viewOfTimeAndPlace(x) {
+    var meetingTime = $("<div id='meeting-time'/>");
+    var meetingLoc = $("<div id='meeting-location'/>");
+    var view = $("<div/>")
+      .append(meetingTime)
+      .append(meetingLoc);
 
     var t1 = date.ofString(x.start);
     var t2 = date.ofString(x.end);
-
-    var row1 = $("<div class='day-text'/>")
-      .text(date.weekDay(t1) + " ")
-      .appendTo(view);
-
-    var row2 = $("<div class='date-text'/>")
-      .text(date.dateOnly(t1))
-      .appendTo(view);
 
     var fromTime = wordify(date.timeOnly(t1));
     var toTime = wordify(date.timeOnly(t2));
@@ -72,62 +89,112 @@ var guestTask = function() {
     if (fromTime.charAt(fromTime.length-2) === toTime.charAt(toTime.length-2))
       fromTime = fromTime.substr(0, fromTime.length-3);
 
-    var row3 = $("<div class='time-text'/>")
-      .append(html.text("from "))
-      .append($("<b>").text(fromTime))
-      .append(html.text(" to "))
-      .append($("<b>").text(toTime))
-      .appendTo(view);
-
-    var row4 = $("<div class='time-text-short hide'/>")
-      .append(html.text("at "))
-      .append($("<b>").text(date.timeOnly(t1)))
-      .appendTo(view);
+    meetingTime
+      .append(date.weekDay(t1).substring(0,3) + ", ")
+      .append(date.dateOnly(t1) + " from ")
+      .append($("<span id='time-text'>" + fromTime + " to " + toTime + "</span>"));
 
     var locText = locationText(x.location);
-    var locDiv = $("<div class='loc-text'/>");
-    var pin = $("<img class='pin'/>");
-      pin.appendTo(locDiv);
-    svg.loadImg(pin, "/assets/img/pin.svg");
     if (locText) {
-      locDiv.append(html.text(locText))
-            .appendTo(view);
+      meetingLoc.append("at ")
+                .append(html.text(locText));
     } else {
-      locDiv.append("Location TBD")
-            .addClass("tbd")
-            .appendTo(view);
+      meetingLoc.append("Location TBD")
+                .addClass("tbd")
     }
 
     return view;
   }
 
+
+/*** SAVE FOR MEETING OPTIONS ***/
+
+  // function viewOfCalendarSlot(x) {
+  //   var view = $("<div class='sug-details'/>");
+
+  //   var t1 = date.ofString(x.start);
+  //   var t2 = date.ofString(x.end);
+
+  //   var row1 = $("<div class='day-text'/>")
+  //     .text(date.weekDay(t1) + " ")
+  //     .appendTo(view);
+
+  //   var row2 = $("<div class='date-text'/>")
+  //     .text(date.dateOnly(t1))
+  //     .appendTo(view);
+
+  //   var fromTime = wordify(date.timeOnly(t1));
+  //   var toTime = wordify(date.timeOnly(t2));
+
+  //   if (fromTime.charAt(fromTime.length-2) === toTime.charAt(toTime.length-2))
+  //     fromTime = fromTime.substr(0, fromTime.length-3);
+
+  //   var row3 = $("<div class='time-text'/>")
+  //     .append(html.text("from "))
+  //     .append($("<b>").text(fromTime))
+  //     .append(html.text(" to "))
+  //     .append($("<b>").text(toTime))
+  //     .appendTo(view);
+
+  //   var row4 = $("<div class='time-text-short hide'/>")
+  //     .append(html.text("at "))
+  //     .append($("<b>").text(date.timeOnly(t1)))
+  //     .appendTo(view);
+
+  //   var locText = locationText(x.location);
+  //   var locDiv = $("<div class='loc-text'/>");
+  //   var pin = $("<img class='pin'/>");
+  //     pin.appendTo(locDiv);
+  //   svg.loadImg(pin, "/assets/img/pin.svg");
+  //   if (locText) {
+  //     locDiv.append(html.text(locText))
+  //           .appendTo(view);
+  //   } else {
+  //     locDiv.append("Location TBD")
+  //           .addClass("tbd")
+  //           .appendTo(view);
+  //   }
+
+  //   return view;
+  // }
+
   mod.loadTask = function(ta) {
     profile.profilesOfTaskParticipants(ta).done(function(profs) {
-      var taskView = $("#task-content");
+      var taskView = $("#guest-task-content");
       taskView.children().remove();
+      var taskWelcome = $("#guest-task-welcome");
+      taskWelcome.children().remove();
 
       var myName = profile.fullName(profs[login.me()].prof);
-      taskView.append($("<p/>").text("Hello, " + myName));
-      taskView.append($("<p/>").text(ta.task_status.task_title));
+      taskWelcome.append($("<p/>").text("Hello, " + myName));
+      taskView.append($("<p id='guest-task-title'/>").text(ta.task_calendar_title));
 
       if ("Scheduling" === variant.cons(ta.task_data)) {
         var state = ta.task_data[1];
         if (state.reserved) {
-          taskView.append(viewOfCalendarSlot(state.reserved.slot));
+          taskView.append(viewOfTimeAndPlace(state.reserved.slot));
+          taskView.append(calendarOptions);
+          taskView.append(googleMapsLink(state.reserved.slot));
           taskView.append(googleCalendarLink(
-              googleCalendarURL(ta.task_status.task_title,
-                                ta.task_calendar_title,
+              googleCalendarURL(ta.task_calendar_title,
+                                window.location,
                                 state.reserved.slot)));
         }
       }
 
-      taskView.append($("<p/>").text("Guests"));
+      taskView.append($("<div class='task-section-header'/>").text("GUESTS"));
       var participantListView = $("<ul/>");
       list.iter(ta.task_participants.organized_for, function(uid) {
         var name = profile.fullName(profs[uid].prof);
-        participantListView.append($("<li/>").text(name));
+        participantListView.append($("<li class='guest-name-row'/>").text(name));
       });
       taskView.append(participantListView);
+
+      taskView.append($("<div class='task-section-header'/>").text("NOTES"));
+      taskView.append("This feature is coming soon.");
+
+      taskView.append($("<div class='task-section-header'/>").text("MESSAGES"));
+      taskView.append("This feature is coming soon.");
 
       observable.onTaskModified.observe("guest-task", mod.loadTask);
     });
