bddd1d7d7194866dc0ed78537184bcf5d79deeb6
Martin Jambon <martin@mjambon.com>
2014-03-05 13:50:04 -0800

    Got rid of "how soon" in default email of step 3 since this field is no longer available from step 2.

--- js/sched3.js
+++ js/sched3.js
@@ -10,12 +10,12 @@ var sched3 = (function() {
     });
   }
 
-  function formalEmailBody(organizerName, hostName, toName, howSoon) {
+  function formalEmailBody(organizerName, hostName, toName) {
     return "Dear "+toName+",\n\n"+
 
     "I'm writing on behalf of "+hostName+" who respectfully requests "+
     "a meeting with you. "+
-    hostName+"'s schedule has the following open times "+howSoon+". "+
+    hostName+"'s schedule has the following open times. "+
     "If any of these times are agreeable, please respond to this e-mail "+
     "with your choice. If you have an assistant or scheduler, I'd be happy "+
     "to follow up with him or her.";
@@ -130,18 +130,6 @@ var sched3 = (function() {
       });
   }
 
-  function textOfHowSoon(x) {
-    var days = x / 86400;
-    if (days > 9.5)
-      return "within two weeks";
-    else if (days >= 5.5)
-      return "within one week";
-    else if (days >= 1.5)
-      return "within "+Math.floor(days + 0.5)+" days";
-    else
-      return "within one day";
-  }
-
   function showEndTime() {
     $("#sched-availability-message-readonly .time-text")
       .removeClass("hide");
@@ -182,8 +170,7 @@ var sched3 = (function() {
     })
   }
 
-  function preFillAvailabilityModal(chats, profs, task,
-                                    howSoon, options, toUid) {
+  function preFillAvailabilityModal(chats, profs, task, options, toUid) {
     var toObsProf = profs[toUid];
 
     loadRecipients(toObsProf);
@@ -194,8 +181,7 @@ var sched3 = (function() {
     var organizerName = profile.fullName(profs[login.me()].prof);
     var hostName = profile.fullName(profs[login.leader()].prof);
     var toName = profile.fullName(toObsProf.prof);
-    var howSoon = textOfHowSoon(howSoon);
-    var body = formalEmailBody(organizerName, hostName, toName, howSoon);
+    var body = formalEmailBody(organizerName, hostName, toName);
     $("#sched-availability-message")
       .val(body);
 
@@ -245,7 +231,6 @@ var sched3 = (function() {
     svg.loadImg(drag, "/assets/img/drag.svg");
 
     var state = sched.getState(task);
-    var howSoon = state.meeting_request.how_soon;
     var options = state.calendar_options;
 
     var availabilityModal = $("#sched-availability-modal");
@@ -254,7 +239,7 @@ var sched3 = (function() {
     }
 
     function composeEmail() {
-      preFillAvailabilityModal(chats, profs, task, howSoon, options, uid);
+      preFillAvailabilityModal(chats, profs, task, options, uid);
       availabilityModal.modal({});
     }
 
