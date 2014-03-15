e75f55bf8f504bb8ab6144f3ac8138674658bb2c
Nicholas Chen <nick@esper.co>
2014-03-15 14:12:54 -0700

    added task actions

--- js/home.js
+++ js/home.js
@@ -24,6 +24,46 @@ var home = (function() {
     return "Email subject line";
   }
 
+  function loadTaskActions(ta) {
+    var view = $("#task-actions");
+    view.children().remove();
+
+    log("about to load");
+
+    var rescheduleAction = $("<div class='task-action'/>");
+    var rescheduleIcon = $("<img/>")
+      .appendTo(rescheduleAction);
+    svg.loadImg(rescheduleIcon, "/assets/img/to-do.svg");
+    rescheduleAction.append(($("<span class='task-action-label'/>"))
+                      .text("Reschedule..."))
+                    .appendTo(view);
+    var cancelAction = $("<div class='task-action'/>");
+    var cancelIcon = $("<img/>")
+      .appendTo(cancelAction);
+    svg.loadImg(cancelIcon, "/assets/img/to-do.svg");
+    cancelAction.append(($("<span class='task-action-label'/>"))
+                  .text("Cancel..."))
+                .appendTo(view);
+    var deleteAction = $("<div class='task-action'/>");
+    var deleteIcon = $("<img/>")
+      .appendTo(deleteAction);
+    svg.loadImg(deleteIcon, "/assets/img/to-do.svg");
+    deleteAction.append(($("<span class='task-action-label'/>"))
+                  .text("Delete..."))
+                .appendTo(view);
+
+    // rescheduleAction.click(function() {
+
+    // })
+    // cancelAction.click(function() {
+
+    // })
+    deleteAction.click(function() {
+      api.archiveTask(ta.tid);
+      observable.onTaskArchived.notify(ta.tid);
+    })
+  }
+
   function viewOfTaskRow(ta) {
     var view = $("<div/>",{id:"task-" + ta.tid})
       .click(function() {
@@ -34,44 +74,19 @@ var home = (function() {
         $("#stats").addClass("hide");
         $("#search-stats").addClass("hide");
         $("#task-actions").removeClass("hide");
+        loadTaskActions(ta);
       })
-
-    // Add both Archive button and Restore button to the row.
-    // By css magic they will only show up in the appropriate tabs.
-
-    // var archiveDiv = $("<div class='archive-div'></div>")
-    //   .appendTo(view);
-    // var archive = $("<img class='archive'/>")
-    //   .appendTo(archiveDiv);
-    // svg.loadImg(archive, "/assets/img/x.svg");
-    // archiveDiv
-    //   .tooltip({"title":"Archive"})
-    //   .click(function() {
-    //     api.archiveTask(ta.tid);
-    //     observable.onTaskArchived.notify(ta.tid);
-    //   });
-
-    // var restoreButton = $("<button class='restore-div'/>")
-    //   .text("Restore")
-    //   .appendTo(view);
-    // restoreButton.click(function() {
-    //   $("#tasks").prepend(view);
-    //   setTaskViewClass(view, classOfActiveTask(ta));
-    //   api.rankTaskFirst(ta.tid);
-    // });
-
-
 
     var newLabel = $("<div class='new-label hide'>NEW</div>");
     var unreadLabel = $("<div class='unread-label'></div>");
-    var toDo = $("<img/>");
-    var toDoLabel = $("<div class='to-do-label hide'/>")
-      .append(toDo);
-    svg.loadImg(toDo, "/assets/img/to-do.svg");
-    var sent = $("<img/>");
-    var sentLabel = $("<div class='sent-label hide'/>")
-      .append(sent);
-    svg.loadImg(sent, "/assets/img/reply.svg");
+    var toDoLabel = $("<div class='to-do-label hide'/>");
+    var toDoIcon = $("<img/>")
+      .appendTo(toDoLabel);
+    svg.loadImg(toDoIcon, "/assets/img/to-do.svg");
+    var sentLabel = $("<div class='sent-label hide'/>");
+    var sentIcon = $("<img/>")
+      .appendTo(sentLabel);
+    svg.loadImg(sentIcon, "/assets/img/reply.svg");
     var taskLabel = $("<div class='task-label'/>")
       .append(newLabel)
       .append(unreadLabel)
@@ -98,25 +113,25 @@ var home = (function() {
 
       var statusRow = $("<div class='task-status'/>")
         .appendTo(taskDetails);
-      var guestsIcon = $("<img/>");
-      var guests = $("<div class='status-icon guests-status-icon'/>")
-        .append(guestsIcon);
+      var guests = $("<div class='status-icon guests-status-icon'/>");
+      var guestsIcon = $("<img/>")
+        .appendTo(guests);
       svg.loadImg(guestsIcon, "/assets/img/group.svg");
-      var emailIcon = $("<img/>");
-      var email = $("<div class='status-icon email-status-icon hide'/>")
-        .append(emailIcon);
+      var email = $("<div class='status-icon email-status-icon hide'/>");
+      var emailIcon = $("<img/>")
+        .appendTo(email);
       svg.loadImg(emailIcon, "/assets/img/email.svg");
-      var optionsIcon = $("<img/>");
-      var options = $("<div class='status-icon options-status-icon hide'/>")
-        .append(optionsIcon);
+      var options = $("<div class='status-icon options-status-icon hide'/>");
+      var optionsIcon = $("<img/>")
+        .appendTo(options);
       svg.loadImg(optionsIcon, "/assets/img/to-do.svg");
-      var calendarIcon = $("<img/>");
-      var calendar = $("<div class='status-icon calendar-status-icon hide'/>")
-        .append(calendarIcon);
+      var calendar = $("<div class='status-icon calendar-status-icon hide'/>");
+      var calendarIcon = $("<img/>")
+        .appendTo(calendar);
       svg.loadImg(calendarIcon, "/assets/img/reply.svg");
-      var reminderIcon = $("<img/>");
-      var reminder = $("<div class='status-icon reminder-status-icon hide'/>")
-        .append(reminderIcon);
+      var reminder = $("<div class='status-icon reminder-status-icon hide'/>");
+      var reminderIcon = $("<img/>")
+        .appendTo(reminder);
       svg.loadImg(reminderIcon, "/assets/img/to-do.svg");
       var statusIcon = $("<div class='task-status-icon'/>")
         .append(guests)
@@ -695,7 +710,7 @@ var home = (function() {
              .addClass("closed");
         filters.slideUp("fast", function() {
           $("#home-content")
-            .attr("style","padding-top:123px");
+            .attr("style","padding-top:163px");
         });
       }
     })
@@ -735,8 +750,7 @@ var home = (function() {
     util.afterTyping(search, 100, updateSearch);
 
     clear.click(function() {
-      search.val("")
-            .focus();
+      search.val("");
       updateSearch();
     })
 
@@ -810,11 +824,7 @@ var home = (function() {
   mod.load = function() {
     loadHeader();
     loadFilters();
-    // loadNavHeader();
     loadTasks();
-    $(".place-nav")
-      .off('click')
-      .click(places.load);
     $(".settings-nav")
       .off('click')
       .click(settings.load);
