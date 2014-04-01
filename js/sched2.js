0b9238dd3a115f5141ca48b46508415a5effeb46
Martin Jambon <martin@mjambon.com>
2014-04-01 16:36:57 -0700

    "Where" section before "When"

--- js/sched2.js
+++ js/sched2.js
@@ -849,6 +849,12 @@ var sched2 = (function() {
                class="hide form-control custom-type-input"/>
       </div>
     </div>
+    <div #whereSection
+         class="edit-info-row">
+      <div class="info-label where-label-edit">WHERE</div>
+      <div #locationContainer
+           class="info"/>
+    </div>
     <div class="edit-info-row">
       <div class="info-label when-label-edit">WHEN</div>
       <div class="info">
@@ -871,12 +877,6 @@ var sched2 = (function() {
           </span>
         </span>
       </div>
-    </div>
-    <div #whereSection
-         class="edit-info-row">
-      <div class="info-label where-label-edit">WHERE</div>
-      <div #locationContainer
-           class="info"/>
     </div>
     <div class="edit-info-row">
       <div class="info-label notes-label-edit">NOTES</div>
