958ffb496fbac3f3b1bb124cb1b25548a946da21
Martin Jambon <martin@mjambon.com>
2014-04-02 11:43:45 -0700

    type.set now checks that the field _type doesn't exist already. type.cast doesn't care if _type already exists but checks for the validity of its arguments (string, object).

--- js/type.js
+++ js/type.js
@@ -15,6 +15,7 @@ var type = (function() {
 
   var typeNameField = "_type";
 
+  /* Protect against redefinition of undefined */
   var undef;
 
   function error(s) {
@@ -25,12 +26,19 @@ var type = (function() {
     return typeof x === "string";
   }
 
+  /* Protect against later redefinition of Object */
+  var Obj = Object;
+
+  function isObject(x) {
+    return x instanceof Obj;
+  }
+
   /* Check the _type field of a Javascript object */
   mod.check = function(t, x) {
     if (! isString(t))
-      throw new TypeError("Invalid 1st argument passed to typecheck.check.");
+      error("Invalid 1st argument passed to typecheck.check.");
     else if (x === null)
-      throw new TypeError("null was found where type " + t + " was expected.");
+      error("null was found where type " + t + " was expected.");
     else if (x === undef)
       error("undefined was found where type " + t + " was expected.");
     else {
@@ -55,13 +63,42 @@ var type = (function() {
       return x;
     else
       return mod.check(t, x)
-  }
+  };
 
-  /* Set the _type field of a Javascript object */
-  mod.set = function(t, x) {
+  /*
+    Set the _type field of a Javascript object,
+    ignoring existing type field if any.
+  */
+  mod.cast = function(t, x) {
+    if (! isString(t))
+      error("Attempt to assign a type name that is not a string.");
+
+    if (! isObject(x))
+      error("Attempt to assign type name "
+            + JSON.stringify(t)
+            + " to a non-object.");
+
     x[typeNameField] = t;
     return x;
-  }
+  };
+
+  /*
+    Set the _type field of a Javascript object,
+    failing if already set (even to the same type)
+  */
+  mod.set = function(t, x) {
+    var t0 = x[typeNameField];
+    if (t0 !== undef) {
+      var s = "<not a string>";
+      if (isString(t0))
+        s = JSON.stringify(t0);
+      error("Attempt to assign type " + t
+            + " to object which already has a " + typeNameField
+            + " field (" + s + ").");
+    }
+    else
+      return mod.cast(t, x);
+  };
 
   return mod;
 })();
