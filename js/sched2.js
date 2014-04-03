e95cb46fe59e05e01b94e2842109689bed2a3355
Martin Jambon <martin@mjambon.com>
2014-04-02 17:11:01 -0700

    Hide "Where" section for phone calls.

--- js/sched2.js
+++ js/sched2.js
@@ -767,13 +767,10 @@ var sched2 = (function() {
 
   function adaptToMeetingType(form, locationForm, slot) {
     setupTimezoneLink(form, locationForm, slot);
-    switch (slot.meeting_type) {
-    case "Call":
-      form.whereSection.addClass("hide");
-      break;
-    default:
+    if (sched.showLocation(slot.meeting_type))
       form.whereSection.removeClass("hide");
-    }
+    else
+      form.whereSection.addClass("hide");
   }
 
   /* Create modal containing date/time picker based on the
