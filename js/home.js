794931a5eeee97f71f0735afe3972d13be8ba102
Pierre-Malo Deniélou <malo@doc.ic.ac.uk>
2014-05-21 15:55:00 -0700

    modularisation

--- js/home.js
+++ js/home.js
@@ -558,74 +558,6 @@ var home = (function() {
     $(".page-title").text("Meetings");
   }
 
-  function cmpEvents(event1,event2) {
-    if ((util.isNotNull(event1.start.dateTime) &&
-         util.isNotNull(event2.start.dateTime) &&
-         event1.start.dateTime > event2.start.dateTime) ||
-        (util.isNotNull(event1.start.date) &&
-         util.isNotNull(event2.start.date) &&
-         event1.start.date > event2.start.date)
-       )
-      return 1;
-    if ((util.isNotNull(event1.start.dateTime) &&
-         util.isNotNull(event2.start.dateTime) &&
-         event1.start.dateTime < event2.start.dateTime) ||
-        (util.isNotNull(event1.start.date) &&
-         util.isNotNull(event2.start.date) &&
-         event1.start.date < event2.start.date))
-      return -1;
-    return 0;
-  }
-
-  function eventToAgenda(event){
-      var summary = "• " + event.summary + "";
-
-      var time = "";
-      if(util.isNotNull(event.start.dateTime)){
-          var d1 = date.ofString(event.start.dateTime);
-          var d2 = date.ofString(event.end.dateTime);
-          time = "\n → " + date.hourRange(d1,d2);
-      }
-      var location = "";
-      if(util.isNotNull(event.location)){
-          location = "\n → " + event.location
-      }
-      var description = "";
-      if(util.isNotNull(event.description)){
-          description = "\n → " + event.description
-      }
-      return (summary + time + location + description + "\n\n");
-  }
-
-
-  function buildAgenda(day, events) {
-    var dayEvents = [];
-    var shortEvents = [];
-    list.iter(events,function(event){
-      if(util.isNotNull(event.start.dateTime)){
-        shortEvents.push(event);
-      } else {
-        dayEvents.push(event);
-      }
-    });
-    dayEvents = list.sort(dayEvents, cmpEvents);
-    shortEvents = list.sort(shortEvents, cmpEvents);
-    var agenda = "";
-    if(dayEvents.length > 0) { agenda = "Day events\n\n"; }
-    list.iter(dayEvents,function(event){
-      agenda = agenda + eventToAgenda(event);
-    });
-    if(shortEvents.length > 0) {
-        agenda = agenda + "Detailed Schedule\n\n";
-        list.iter(shortEvents,function(event){
-            agenda = agenda + eventToAgenda(event);
-        });
-    } else {
-        agenda = agenda + "\nNo scheduled events for today";
-    }
-    return agenda;
-  }
-
   mod.load = function() {
 '''
 <div #logout>
@@ -634,89 +566,14 @@ var home = (function() {
 <div #revoke>
   <a href="#">Revoke Esper access to my Google account</a>
 </div>
-<div #calendarList class="chat-actions clearfix"> Calendars found:</div>
-<div #sendAgenda>
-  <span> Date: <input #agendaDatePicker type="text"/> </span>
-  <div #agendaDrafter><a href="#">Send Agenda to the Exec</a></div>
-</div>
 '''
     var view = $("#onboarding-interface");
     view.children().remove();
     mod.calendar = {};
 
-    sendAgenda.addClass("hide");
-    agendaDatePicker.datepicker({inline: true,
-                                 currentText: "Today",
-                                 gotoCurrent: true});
-    agendaDatePicker.css("width","100px");
-    var calList = $("#calendarList");
-
     revoke.click(function() {
       api.postGoogleAuthRevoke().done(revoke.remove());
       return false;
-    });
-
-    function diplayCalendarChoice(data) {
-      var calendarlist = data.items;
-      list.iter(calendarlist, function(x) {
-'''
-<input #calendarRadio type="radio" name="calendar">
-<label #calendarLabel></label>
-'''
-            calendarRadio.attr("id", "calendar-" + x.id);
-            calendarLabel.attr("value", x.summary);
-            calendarLabel.attr("for", "calendar-" + x.id);
-            calendarRadio.attr("value", x.summary);
-            calendarLabel.append(x.summary);
-            calendarList.append(calendarRadio);
-            calendarList.append(calendarLabel);
-            log(x.summary + " appended");
-          calendarRadio.change(function(event) {
-            log("radio change to" + x.summary);
-            mod.calendar = x;
-          });
-        });
-        view.append(calendarList);
-        view.append(sendAgenda);
-        //view.append(selectedCalendar);
-
-        //var radioChoice = calendarChoice(calendarlist);
-        sendAgenda.removeClass("hide");
-        calendarList.button();
-    }
-
-    agendaDrafter.click(function() {
-      var chosendate = agendaDatePicker.datepicker("getDate");
-      if (chosendate && mod.calendar != ""){
-        log("calendar is " + mod.calendar);
-        log("date is " + chosendate + " or " + chosendate.getTime());
-        api.getCalendarAgenda(login.leader(),
-                              mod.calendar.id,
-                              chosendate.toISOString()).
-              done(function (eventlist) {
-                  //log(eventlist);
-                  var agenda = buildAgenda(date,eventlist.items);
-                  var url = "https://mail.google.com/mail?view=cm&cs=wh&tf=0";
-                  api.getEmails(login.leader(), login.getTeam().teamid)
-                      .done(function (x) {
-                        log(x.account_emails[0].email);
-                        if (encodeURIComponent(agenda).length > 1850) {
-                            while(encodeURIComponent(agenda).length > 1850) {
-                                log("Long agenda detected! Shortening ...");
-                                agenda = agenda.substring(0,agenda.length-10);
-                            };
-                            agenda = agenda + " [...]";
-                        }
-                        open(url + "&to="
-                             + encodeURIComponent(x.account_emails[0].email)
-                             + "&su="
-                             + encodeURIComponent("Agenda for "
-                                                  + date.dateOnly(chosendate))
-                             + "&body="
-                             + encodeURIComponent(agenda));
-                      });
-              });
-      };
     });
 
     api.getGoogleAuthInfo(document.URL)
@@ -725,8 +582,7 @@ var home = (function() {
         view.append(logout);
         if (info.has_token) view.append(revoke);
         else window.location = info.google_auth_url;
-        api.getCalendarList(login.leader(), null)
-              .done(diplayCalendarChoice);
+        agenda.create(view);
       });
   };
 
