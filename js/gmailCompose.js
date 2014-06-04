800bc5ddb798d7f49213ceaf8ee012b97d80c531
Martin Jambon <martin@mjambon.com>
2014-06-04 14:49:11 -0700

    Missing file

---
+++ js/gmailCompose.js
@@ -0,0 +1,24 @@
+/*
+  Open a Gmail composition window
+*/
+
+var gmailCompose = (function() {
+  var mod = {};
+
+  function makeUrl(param) {
+    var url = "https://mail.google.com/mail?view=cm&cs=wh&tf=0";
+    if (util.isString(param.to))
+      url += "&to=" + encodeURIComponent(param.to);
+    if (util.isString(param.subject))
+      url += "&su=" + encodeURIComponent(param.subject);
+    if (util.isString(param.body))
+      url += "&body=" + encodeURIComponent(param.body);
+    return url;
+  };
+
+  mod.compose = function(param) {
+    window.open(makeUrl(param));
+  };
+
+  return mod;
+})();
