e31c97d044615a8d189bc53876451b73c5fea23f
Martin Jambon <martin@mjambon.com>
2014-08-12 10:43:55 -0700

    Reformatting

--- gmail-is/Menu.ts
+++ gmail-is/Menu.ts
@@ -40,19 +40,19 @@ module Esper.Menu {
       return false;
   }
 
-function popWindow(url, width, height) {
-  var leftPosition, topPosition;
-  //Allow for borders.
-  leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
-  //Allow for title and status bars.
-  topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+  function popWindow(url, width, height) {
+    /* Allow for borders. */
+    var leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+    /* Allow for title and status bars. */
+    var topPosition = (window.screen.height / 2) - ((height / 2) + 50);
 
-  window.open(url, "Window2", "status=no,height="
-    + height + ",width=" + width + ",resizable=yes,left="
-    + leftPosition + ",top=" + topPosition + ",screenX="
-    + leftPosition + ",screenY=" + topPosition
-    + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
-}
+    window.open(
+      url, "Window2", "status=no,height="
+        + height + ",width=" + width + ",resizable=yes,left="
+        + leftPosition + ",top=" + topPosition + ",screenX="
+        + leftPosition + ",screenY=" + topPosition
+        + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
+  }
 
   function makeActionLink(text, action, danger) {
     var link = $("<li class='esper-li'/>")
