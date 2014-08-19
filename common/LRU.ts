95ccd0f9c8675a1291ca091d301ffcee7de3b8e7
Martin Jambon <martin@mjambon.com>
2014-08-19 15:25:46 -0700

    Naive implementation of an LRU cache suitable for keeping the last few active items.

---
+++ common/LRU.ts
@@ -0,0 +1,68 @@
+/*
+  Small-size least-recently-used cache (LRU).
+*/
+
+module Esper.LRU {
+  export class C<T> {
+    /*
+      Array of at most maxlen elements without repeats,
+      least recently added last.
+    */
+    all: T[] = [];
+
+    private maxlen: number;
+    private eq: (a: T, b: T) => boolean;
+
+    constructor(maxlen: number,
+                eq: (a: T, b: T) => boolean) {
+      this.maxlen = maxlen;
+      this.eq = eq;
+    }
+
+    remove(x: T) {
+      var i = 0;
+      while (i < this.all.length) {
+        if (this.eq(x, this.all[i]))
+          this.all.splice(i, 1);
+        else
+          i++;
+      }
+    }
+
+    add(x: T) {
+      this.remove(x);
+      this.all.unshift(x);
+      if (this.all.length > this.maxlen) {
+        this.all.pop();
+      }
+    }
+
+    clear() {
+      this.all.splice(0, this.all.length);
+    }
+  }
+
+  export function test() {
+    var cache = new Esper.LRU.C(3, function(a, b) { return a == b; });
+
+    function eq(a) {
+      console.assert(JSON.stringify(cache.all) === JSON.stringify(a));
+    }
+
+    eq([]);
+    cache.add(2);
+    eq([2]);
+    cache.add(2);
+    eq([2]);
+    cache.add(3);
+    eq([3,2]);
+    cache.add(2);
+    eq([2,3]);
+    cache.add(4);
+    eq([4,2,3]);
+    cache.add(3);
+    eq([3,4,2]);
+    cache.add(5);
+    eq([5,3,4]);
+  }
+}
