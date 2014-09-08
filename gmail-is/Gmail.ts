a4a8900b37c58e6fa6525b6527ded60f822ead53
Tikhon Jelvis <tikhon@jelv.is>
2014-09-08 10:53:27 -0700

    Extracted the logic for trying to find an anchor point into a function.

--- gmail-is/Gmail.ts
+++ gmail-is/Gmail.ts
@@ -5,19 +5,31 @@
 module Esper.Gmail {
 
   export function findSidebarAnchor() {
-    var anchor = $(".nH.g.id");
-    if (anchor.length !== 1) {
-      Log.e("Cannot find anchor point for the Esper thread controls.");
-      return $();
-    }
-    else
-      return anchor;
+    return find$(".nH.g.id", "Esper thread controls");
   }
   
   /** Returns the tbody that contains the composition toolbar. This is
    *  the bar with the send button, the attachment tool and so on.
    */
   export function compositionToolbar() {
-    return $("\\:qd tbody");
+    return find$("\\:qd tbody", "Esper composition toolbar");
+  }
+
+  /** Tries to find the element described by the given selector. If it
+   *  isn't found, or there is more than one, logs an error and
+   *  returns an empty jQuery selection, otherwise returns one with
+   *  the given element.
+   *
+   *  The component name makes the error message more useful.
+   */
+  function find$(selector, componentName) {
+    var anchor = $(selector);
+
+    if (anchor.length !== 1) {
+      Log.e("Cannot find anchor point for the " + componentName + ".");
+      return $();
+    } else {
+      return anchor;
+    }
   }
 }
