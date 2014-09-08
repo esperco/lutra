03ddf1b0b0e6760fa47c5dcbf9223225d8e74a76
Tikhon Jelvis <tikhon@jelv.is>
2014-09-08 15:59:02 -0700

    Allowed compositionToolbar to return any number of results.

--- gmail-is/Gmail.ts
+++ gmail-is/Gmail.ts
@@ -5,31 +5,22 @@
 module Esper.Gmail {
 
   export function findSidebarAnchor() {
-    return find$(".nH.g.id", "Esper thread controls");
-  }
-  
-  /** Returns the tbody that contains the composition toolbar. This is
-   *  the bar with the send button, the attachment tool and so on.
-   */
-  export function compositionToolbar() {
-    return find$("\\:qd tbody", "Esper composition toolbar");
-  }
-
-  /** Tries to find the element described by the given selector. If it
-   *  isn't found, or there is more than one, logs an error and
-   *  returns an empty jQuery selection, otherwise returns one with
-   *  the given element.
-   *
-   *  The component name makes the error message more useful.
-   */
-  function find$(selector, componentName) {
-    var anchor = $(selector);
+    var anchor = $(".nH.g.id");
 
     if (anchor.length !== 1) {
-      Log.e("Cannot find anchor point for the " + componentName + ".");
+      Log.e("Cannot find anchor point for the Esper thread controls.");
       return $();
     } else {
       return anchor;
     }
+  }
+  
+  /** Returns the tbody that contains the composition toolbar. This is
+   *  the bar with the send button, the attachment tool and so
+   *  on. Since there can be any number of these on the page at a
+   *  time, this can return any number of elements.
+   */
+  export function compositionToolbar() {
+    return $("div.aDg div.aDh");
   }
 }
