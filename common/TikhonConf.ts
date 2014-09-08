7f514dc21b8897d103b2bc4c730aa18a34367414
Tikhon Jelvis <tikhon@jelv.is>
2014-09-08 14:10:12 -0700

    Removed autogenerated TikhonConf.ts which was included by accident.

--- common/TikhonConf.ts
+++
@@ -1,7 +0,0 @@
-module Esper.Conf {
-  export var prod = false;
-  export var version = "0.0.17";
-  export module Api {
-    export var url = "http://ec2-23-20-70-85.compute-1.amazonaws.com";
-  }
-}
