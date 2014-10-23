d34696344670eb5cbe5f1103fef7063d9b8e05e6
Jeff Meister <nanaki@gmail.com>
2014-10-22 18:08:11 -0700

    Scroll the team menu if it's too big

--- gmail-is/Sidebar.ts
+++ gmail-is/Sidebar.ts
@@ -109,6 +109,9 @@ module Esper.Sidebar {
   </div>
 </div>
 '''
+    dropdown.css("max-height", $(window).height());
+    dropdown.css("overflow", "auto");
+
     function onTeamSwitch(toTeam) {
       dismissDropdowns();
       wrap.fadeOut(250);
