5675dc43e73588b45f606b34f6a55dc75921d8fd
Tikhon Jelvis <tikhon@jelv.is>
2015-03-09 14:18:20 -0700

    Implemented the core UI for managing guests in the scheduled group.

--- gmail-is/GroupTab.ts
+++ gmail-is/GroupTab.ts
@@ -2,24 +2,17 @@
  * with group scheduling.
  */
 module Esper.GroupTab {
+
   /** Returns the top-level container for the group scheduling tab. */
   export function container() {
 '''
 <div #container class="esper-tab-container">
-  <div class="esper-tab-header">
-    <h1>
-      Title
-    </h1>
-    <input #title type="text" class="esper-input"></input>
-  </div>
   <div class="esper-section">
     <div class="esper-section-header esper-clearfix esper-open">
       <h1> Guests </h1>
       <span class="esper-hide-section esper-link">Hide</span>
     </div>
-    <div class="esper-section-container">
-      <ul #people class="esper-group-people">
-      </ul>
+    <div #guestSection class="esper-section-container">
     </div>
   </div>
 </div>
@@ -31,60 +24,102 @@ module Esper.GroupTab {
       });
     });
 
-    var availabilities = ["yes", "no", "maybe"];
+    var guests = [{ name : "Peter Esper", availability : GroupScheduling.Availability.none },
+                  { name : "Lois Esper",  availability : GroupScheduling.Availability.none },
+                  { name : "Foo Bar",     availability : GroupScheduling.Availability.none }];
 
-    function nextAvailability(current) {
-      var index = availabilities.indexOf(current);
-      return availabilities[(index + 1) % availabilities.length];
-    }
-
-    function availabilityClass(availability) {
-      return "esper-availability-" + availability;
-    }
-
-    var guests = [{ name : "Peter Esper", availability : "none" },
-                  { name : "Lois Esper", availability : "none" },
-                  { name : "Foo Bar", availability : "none" }];
-
-    guests.forEach(function (guest) {
-      var element = $("<li>").text(guest.name);
-      element.addClass(availabilityClass("none"));
-
-      element.mousedown(function (e) {
-        e.preventDefault()
-        return false;
-      });
-
-      element.click(function () {
-        element.removeClass(availabilityClass(guest.availability));
-        
-        guest.availability = nextAvailability(guest.availability);
-
-        element.addClass(availabilityClass(guest.availability));
-      });
-
-      var close = $("<div class=\"esper-close\">").text("x");
-      close.click(function () {
-        element.slideUp();
-        guest.availability = "none";
-
-        return false;
-      });
-      element.append(close);
-
-      people.append(element);
-    });
+    guestSection.append(guestsList(guests, addGuest));
 
     return container;
-    
+
+    function addGuest(guest) {
+      guests.push(guest);
+    }
+  }
+
+  export function guestsList(guests: GroupScheduling.Guest[],
+                             onAddGuest: (guest:GroupScheduling.Guest) => any) {
+'''
+<ul #list class="esper-group-people">
+  <li #addGuest class="esper-group-add-guests">
+    <input #nameInput type="text"> </input>
+    <div #addButton class="esper-close">+</div>
+  </li>
+</ul>
+'''
+    guests.forEach(function (guest) {
+      addGuestWidget(guest);
+    });
+
+    addButton.click(function () {
+      addNewGuest();
+    });
+
+    nameInput.keyup(function (e) {
+      // Monitoring for the ENTER key
+      if (e.keyCode == 13 && nameInput.val() !== "") {
+        addNewGuest();
+      }
+    });
+
+    return list;
+
+    function addGuestWidget(guest, animate?) {
+      var newWidget = personWidget(guest.name, function () {
+        guest.availability = GroupScheduling.Availability.none;
+      });
+
+      addGuest.before(newWidget);
+
+      if (animate) {
+        newWidget.slideDown("fast");
+      } else {
+        newWidget.show();
+      }
+    }
+
+    function addNewGuest() {
+      var newGuest = {
+        name         : nameInput.val(),
+        availability : GroupScheduling.Availability.none
+      };
+
+      onAddGuest(newGuest);
+      addGuestWidget(newGuest, true);
+      nameInput.val("");
+    }
   }
 
   /** A list element that contains a person's name and can be delete
    *  from the list with an "x".
    */
-  export function personWidget(name: string) {
+  export function personWidget(name: string, onClose) {
 '''
+<li #widget>
+  <span #nameSpan> </span>
+  <div #close class="esper-close">x</div>
+</li>
+'''
+    nameSpan.text(name);
 
-'''
+    widget.mousedown(function (e) {
+      e.preventDefault()
+      return false;
+    });
+
+    close.click(function () {
+      onClose(name);
+      widget.slideUp();
+
+      return false;
+    });
+
+    widget.hide(); // Hidden so that it can be animated in if necessary.
+
+    return widget;
+  }
+
+  function availabilityClass(availability: GroupScheduling.Availability) {
+    return "esper-availability-" + GroupScheduling.Availability[availability];
   }
 }
