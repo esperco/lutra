8133cd8cba77811f25464f978ef4b9dfcb256838
Tikhon Jelvis <tikhon@jelv.is>
2015-03-23 17:37:16 -0700

    Added a function that allows me to create my own promises, conceptually like an ugly callCC.

--- common/Promise.ts
+++ common/Promise.ts
@@ -21,6 +21,18 @@ module Esper.Promise {
     return dfd;
   }
 
+  /** Wrap a function into a promise, passing in resolve and reject as
+   *  functions. Kind of like an ugly callCC.
+   *
+   *  I would write out a full type for this, but TypeScript makes it
+   *  too much of a pain to get correct.
+   */
+  export function wrap<T>(body): JQueryPromise<T> {
+    return new $.Deferred(function (resolve, reject) {
+      body(resolve, reject);
+    });
+  }
+
   /* take a list of deferred computations and
      return a deferred list of the results */
   export function join<T>(a : JQueryPromise<T>[]) : JQueryPromise<T[]> {
