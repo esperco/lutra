52e9c5de20010d2c703596d0ea81f7680a2024f0
ochengo <oliver.hw.cheng@gmail.com>
2015-07-14 18:20:00 -0700

    phone call, not phonecall

--- js/global.js
+++ js/global.js
@@ -229,7 +229,7 @@ function setDonePageFromHash() {
   document.getElementById("user").innerHTML = user;
 
   var email = location.hash.split("#")[1];
-  var intro_email = "https://mail.google.com/mail/u/0/?view=cm&fs=1&to=".concat(email).concat("&su=Nice%20to%20meet%20you,%20").concat(formatted_name).concat("&body=").concat(greeting).concat("%0D%0A%0D%0AGreat%20to%20meet%20you!%20I%27m%20excited%20to%20have%20you%20as%20an%20assistant.%0D%0AI%27d%20love%20to%20speak%20with%20you%20and%20learn%20more%20about%20what%20you%20can%20do%20for%20me.%0D%0ACan%20you%20help%20find%20a%20good%20time%20for%20a%20phonecall%20that%20works%20for%20both%20of%20us?%0D%0A%0D%0ACheers,%0D%0A").concat(user);
+  var intro_email = "https://mail.google.com/mail/u/0/?view=cm&fs=1&to=".concat(email).concat("&su=Nice%20to%20meet%20you,%20").concat(formatted_name).concat("&body=").concat(greeting).concat("%0D%0A%0D%0AGreat%20to%20meet%20you!%20I%27m%20excited%20to%20have%20you%20as%20an%20assistant.%0D%0AI%27d%20love%20to%20speak%20with%20you%20and%20learn%20more%20about%20what%20you%20can%20do%20for%20me.%0D%0ACan%20you%20help%20find%20a%20good%20time%20for%20a%20phone%20call%20that%20works%20for%20both%20of%20us?%0D%0A%0D%0ACheers,%0D%0A").concat(user);
   document.getElementById("mailto-link").innerHTML = email;
   $("#mailto-link").prop("href", intro_email);
   $("#mailto-msg").prop("href", intro_email);
