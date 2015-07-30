5d6c4f0bb79dd92c54f9460765a78db64c232bb8
Tikhon Jelvis <tikhon@jelv.is>
2015-07-30 10:24:32 -0700

    Started working on organizing the slide code.

---
+++ gmail-is/Slides.ts
@@ -0,0 +1,16 @@
+/** A module for working with sliding screens like the followup flow. 
+ *
+ *  The slides are specified as a list of functions that, given a
+ *  uniform state object, initialize each slide. When the user goes
+ *  back, the current slide is completely removed and recreated when
+ *  it's needed, but modifications to the state object are persisted.
+ */
+module Esper.Slides {
+  export function create<T>(startState : T, slides) {
+    
+
+    return {
+      
+    };
+  }
+}
