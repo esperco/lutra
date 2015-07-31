ed5c7c69713498fcdda2a7d3a1246cd55525f75c
Andrew Fong <andrewfong@esper.com>
2015-07-31 08:27:48 +0000

    Move NPM package stuff out into main Otter dir since they affect multiple directories

--- config.js
+++ config.js
@@ -3,29 +3,29 @@
 // Paths should be relate to config file unless otherwise specified
 module.exports = {
 
-  // Location of this file relative to the base dir for Otter -- this is
-  projectBase: "..",
+  // Location of this file relative to the base dir for Otter
+  projectBase: ".",
 
   // Path to oblivion executable for pre-processing TS files. Oblivion is not
   // provided via Gulp or NPM, and we assume Oblivion has been correctly
   // setup already via the Makefile
-  oblivionPath: "./bin/oblivion",
+  oblivionPath: "setup/bin/oblivion",
 
   // Path to TypeScript compiler command
   tscPath: "tsc",
 
   // Glob patterns for finding all TS files to build / watch
-  tsGlobs: [ "ts/*.d.ts", "../ts/*.ts" ],
+  tsGlobs: [ "setup/ts/*.d.ts", "ts/*.ts" ],
 
   // A TS file that references all other TS files -- use this file to control
   // the order in which files are concatenated or referenced
-  tsEntryPoint: "../ts/Aardvark.ts",
+  tsEntryPoint: "ts/Aardvark.ts",
 
   // Bundle TS into this file in pubDir
   tsBundleName: "js/app.js",
 
   // Distributable HTML, JS, etc.
-  pubDir: "../pub",
+  pubDir: "pub",
 
   // Production mode? => this can be set to true within the gulpfile itself
   // by calling the "production" task
