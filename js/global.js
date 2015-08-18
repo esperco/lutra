52192366779a945d7efa01f530d1dfe99085350c
Andrew Fong <andrewfong@esper.com>
2015-08-18 17:26:41 +0000

    Segment tracking on each static page

--- js/global.js
+++ js/global.js
@@ -87,6 +87,7 @@ function loadPricingDescription() {
 }
 
 function main() {
+  analytics.page();
   loadElements();
   loadPricingDescription();
   resizer();
@@ -94,3 +95,9 @@ function main() {
  }
 
 $(document).ready(main);
+
+// NB: Analytics.js (Segment) should load ASAP -- don't wait for document ready
+// Copied and pasted from https://segment.com/docs/libraries/analytics.js/quickstart/
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
+  analytics.load("ZxAl2zWkKLGVJesGQiIxEU1JKm0dPNd8");
+}}();
