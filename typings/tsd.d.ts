a7c950abb671e70e8860090c31921987a3110bc6
Andrew Fong <andrewfong@esper.com>
2015-08-19 21:06:04 +0000

    Add TSD files + typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,11 @@
+
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="segment-analytics/segment-analytics.d.ts" />
+/// <reference path="fullCalendar/fullCalendar.d.ts" />
+/// <reference path="jqueryui/jqueryui.d.ts" />
+/// <reference path="chrome/chrome.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="webrtc/MediaStream.d.ts" />
+/// <reference path="cryptojs/cryptojs.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
