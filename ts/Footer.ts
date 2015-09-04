c853c81d5f54369606e068260b0c2006813a4b7b
Andrew Fong <andrewfong@esper.com>
2015-09-04 22:09:47 +0000

    Conform to esper.com footer

--- ts/Footer.ts
+++ ts/Footer.ts
@@ -8,28 +8,31 @@ module Footer {
 Footer.load() can still be called due to default value, only Footer.load(true) will disable. */
 
 export function load(onboarding = false) {
+
+  // NB: Check with Nina and/or Mackenzie before uncommenting Chrome install link
 '''
 <div #view class="footer clearfix">
   <ul class="col-xs-2">
     <li class="footer-header">Esper</li>
-    <li><a href="https://chrome.google.com/webstore/detail/esper/jabkchbdomjjlbahjdjemnnghkakfcog"
-           target="_blank" class="gray-link">Install</a></li>
-    <li><a href="http://esper.com/team.html" target="_blank"
-           class="gray-link">About us</a></li>
+    <!-- <li><a href="https://chrome.google.com/webstore/detail/esper/jabkchbdomjjlbahjdjemnnghkakfcog"
+           target="_blank" class="gray-link">Install</a></li> -->
+    <li><a href="http://esper.com/team" target="_blank"
+           class="gray-link">About</a></li>
+    <li><a href="http://esper.com/jobs" target="_blank"
+           class="gray-link">Careers</a></li>
     <li><a href="http://blog.esper.com" target="_blank"
            class="gray-link">Blog</a></li>
-    <li><a href="http://esper.com/jobs.html" target="_blank"
-           class="gray-link">Jobs</a></li>
+    <li><a href="http://tech.esper.com" target="_blank"
+           class="gray-link">Tech Blog</a></li>
   </ul>
   <ul class="col-xs-2">
     <li class="footer-header">Support</li>
-    <li><a href="http://esper.com/discover.html" target="_blank"
-           class="gray-link">Getting started</a></li>
-    <li><a href="http://esper.com/privacy-policy.html" target="_blank"
+    <li><a href="http://esper.com/contact" target="_blank"
+           class="gray-link">Contact</a></li>
+    <li><a href="http://esper.com/privacy-policy" target="_blank"
            class="gray-link">Privacy</a></li>
-    <li><a href="http://esper.com/terms-of-use.html" target="_blank"
+    <li><a href="http://esper.com/terms-of-use" target="_blank"
            class="gray-link">Terms</a></li>
-    <li><a #contact href="#" class="gray-link">Contact us</a></li>
   </ul>
   <div class="col-xs-8">
     <a #wordMarkContainer href="http://esper.com" target="_blank"
@@ -40,10 +43,6 @@ export function load(onboarding = false) {
     var wordMark = $("<img class='svg-block word-mark'/>")
       .appendTo(wordMarkContainer);
     Svg.loadImg(wordMark, "/assets/img/word-mark.svg");
-
-    contact.click(function() {
-      GmailCompose.makeUrl({ to: "team@esper.com" });
-      });
 
     if (onboarding) {
       view.hide();
