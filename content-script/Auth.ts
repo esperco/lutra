4dddb43b0c219914f4a0afa2a8549ac6a2d1ede8
Andrew Fong <andrewfong@esper.com>
2015-09-25 17:41:11 +0000

    Fix errors when logging out

--- content-script/Auth.ts
+++ content-script/Auth.ts
@@ -82,13 +82,14 @@ module Esper.Auth {
     EsperStorage.loadCredentials(
       googleAccountId,
       function(x: Types.Account) {
-        if (x.credentials !== undefined
-            || (x.declined === true && ! forceLogin)) {
-          Login.setAccount(x);
-          sendCredentialsResponse(x);
-        }
-        else {
-          openWelcomeModal(x);
+        if (!x.declined || forceLogin) {
+          if (x.credentials !== undefined) {
+            Login.setAccount(x);
+            sendCredentialsResponse(x);
+          }
+          else {
+            openWelcomeModal(x);
+          }
         }
     });
   }
