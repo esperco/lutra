2530ca5aac2cc41973edcdc6a793a7fb4e7fd785
v2kovac <vk.kovac@gmail.com>
2015-09-29 14:56:04 -0700

    better definition

--- typings/quill/quill.d.ts
+++ typings/quill/quill.d.ts
@@ -1,6 +1,11 @@
 interface QuillStatic {
-    new(container1: Element, options?: Object): any;
+    new(container1: Element, options?: Object): Delta;
+}
+
+interface Delta {
     getContents(start?: number, end?: number): Object;
-    setContents(delta: Object, source: Object): void;
+    setContents(delta: Object, source?: string): void;
     getHTML(): string;
+    setText(html: string, source?: string): void;
+    on(arg: string, arg2: Function): void;
 }
