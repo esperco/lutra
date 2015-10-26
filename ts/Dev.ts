538be3abc075feb59455d6977bf9adef73de9f19
Andrew Fong <andrewfong@esper.com>
2015-10-26 16:49:36 +0000

    Rename test file

--- ts/Dev.ts
+++ ts/Dev.ts
@@ -1,5 +1,5 @@
 /// <reference path="./Main.ts" />
-/// <reference path="./Main.Test.ts" />
+/// <reference path="./Layout.Test.ts" />
 /// <reference path="./Route.Test.ts" />
 
 Esper.PRODUCTION = false;
