dc0a9e700dcf817273c8a51d513592954a98318f
Andrew Fong <andrewfong@esper.com>
2015-10-28 18:18:22 +0000

    More approved domains for dev

--- ts/DevConf.ts
+++ ts/DevConf.ts
@@ -10,7 +10,13 @@ module Esper.Conf {
     "http://esper.com",
     "https://app.esper.com",
     "http://localhost",
-    "http://localhost:5000"
+    "http://localhost:5000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5000",
+    "https://localhost",
+    "https://localhost:5000",
+    "https://127.0.0.1",
+    "https://127.0.0.1:5000"
   ];
 }
 
