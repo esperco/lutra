c5b7d9b9f3cf889a624e255589cdbc62d9cd6f44
Andrew Fong <andrewfong@esper.com>
2015-11-20 20:40:12 +0000

    Add / remove calendars for existing team

--- ts/Calendars.ts
+++ ts/Calendars.ts
@@ -3,6 +3,7 @@
 */
 
 /// <reference path="../marten/ts/Model.StoreOne.ts" />
+/// <reference path="../marten/ts/Queue.ts" />
 /// <reference path="./Esper.ts" />
 /// <reference path="./Teams.ts" />
 
@@ -83,4 +84,58 @@ module Esper.Calendars {
       selectStore.set(defaultSelection());
     }
   }
+
+  export function addTeamCalendar(teamId: string, cal: ApiT.Calendar) {
+    var team = Teams.get(teamId);
+    if (team) {
+      var teamCopy = _.cloneDeep(team); // Store values immutable so clone
+      teamCopy.team_calendars = teamCopy.team_calendars || [];
+      teamCopy.team_calendars.push(cal);
+      cal.calendar_default_view = true;
+      cal.calendar_default_write = true;
+      cal.calendar_default_agenda = true;
+      queueUpdate(teamCopy);
+    }
+  }
+
+  export function removeTeamCalendar(teamId: string, cal: ApiT.Calendar) {
+    var team = Teams.get(teamId);
+    if (team) {
+      var teamCopy = _.cloneDeep(team); // Store values immutable so clone
+      _.remove(teamCopy.team_calendars,
+        (c) => Calendars.getId(c) === Calendars.getId(cal)
+      );
+      queueUpdate(teamCopy);
+    }
+  }
+
+  // Queues update to server to match calendars on a given team object
+  function queueUpdate(team: ApiT.Team) {
+    var _id = team.teamid;
+    nextUpdates[_id] = team.team_calendars;
+
+    /*
+      Prepend queue-id because we only want to be blocking on team calendar
+      updates, not any object with this teamid
+    */
+    var p = Queue.enqueue("team-cal-" + _id, () => {
+      var calendars = nextUpdates[_id];
+      if (calendars) {
+        delete nextUpdates[_id];
+        return Api.putTeamCalendars(_id, calendars);
+      }
+
+      // Return promise that resolves to current team to avoid pushFetch
+      // setting something to null
+      var team = Teams.get(_id);
+      return $.Deferred<ApiT.Team>().resolve(team).promise();
+    });
+
+    Teams.teamStore.pushFetch(team.teamid, p, team);
+  }
+
+  // Track pending calendar updates for team
+  var nextUpdates: {
+    [index: string]: ApiT.Calendar[]
+  } = {};
 }
---
+++ ts/Components.CalAdd.tsx
@@ -0,0 +1,159 @@
+/*
+  Component for adding and sharing new calendars
+*/
+
+/// <reference path="../marten/ts/ReactHelpers.ts" />
+/// <reference path="../marten/ts/ApiC.ts" />
+/// <reference path="../marten/ts/Queue.ts" />
+/// <reference path="./Esper.ts" />
+/// <reference path="./Calendars.ts" />
+/// <reference path="./Teams.ts" />
+/// <reference path="./Components.Modal.tsx" />
+
+module Esper.Components {
+  var Component = ReactHelpers.Component;
+
+  interface CalAddProps {
+    onDone: () => void;
+  }
+
+  interface CalAddState {
+    selectedTeamId?: string;
+  }
+
+  export class CalAdd extends Component<CalAddProps, CalAddState>{
+    constructor(props: CalAddProps) {
+      super(props);
+      var loginInfo = Login.InfoStore.val();
+
+      // Init state = first team (if any)
+      this.state = {
+        selectedTeamId: Teams.first() && Teams.first().teamid
+      };
+
+      // Trigger async -> does nothing if already loaded
+      ApiC.getCalendarList();
+    }
+
+    renderWithData() {
+      var store = ApiC.getCalendarList.store;
+      var key = ApiC.getCalendarList.strFunc([]);
+
+      var calData = store.val(key);
+      var calendars = calData && calData.calendars;
+      var calMeta = store.metadata(key);
+      var dataStatus = calMeta && calMeta.dataStatus;
+
+      if (dataStatus === Model.DataStatus.FETCH_ERROR ||
+          dataStatus === Model.DataStatus.PUSH_ERROR) {
+        return <div className="alert compact alert-danger" role="alert">
+          <i className="fa fa-fw fa-warning"></i>{" "}
+          Whoops. Something broke.{" "}
+          <a href="http://esper.com/contact">
+            Please try contacting us at esper.com/contact.
+          </a>
+        </div>;
+      }
+
+      else if (dataStatus !== Model.DataStatus.READY) {
+        return <div className="esper-spinner esper-centered esper-medium" />;
+      }
+
+      return <div>
+        { this.renderTeamSelector() }
+        <div className="list-group">
+          { _.map(calendars, this.renderCalendar.bind(this)) }
+        </div>
+        { this.renderFooter() }
+      </div>;
+    }
+
+    renderCalendar(cal: ApiT.Calendar) {
+      var isSelected = false;
+      var team = this.getTeam();
+      if (team) {
+        isSelected = !!_.find(team.team_calendars,
+          (c) => Calendars.getId(c) === Calendars.getId(cal)
+        );
+      }
+
+      return <a key={Calendars.getId(cal)} href="#"
+                onClick={() => this.selectCalendar(cal)}
+                className={"list-group-item" + (isSelected ?
+                           " list-group-item-success" : "")}>
+        <i className={"fa fa-fw " + (isSelected ?
+                      "fa-calendar-check-o" : "fa-calendar-o")} />{" "}
+        {cal.calendar_title}
+      </a>;
+    }
+
+    selectCalendar(cal: ApiT.Calendar) {
+      var team = this.getTeam();
+      if (team) {
+        var isSelected = !!_.find(team.team_calendars,
+          (c) => Calendars.getId(c) === Calendars.getId(cal)
+        );
+        if (isSelected) {
+          Calendars.removeTeamCalendar(team.teamid, cal);
+        } else {
+          Calendars.addTeamCalendar(team.teamid, cal);
+        }
+      }
+    }
+
+    getTeam() {
+      return Teams.get(this.state.selectedTeamId);
+    }
+
+    renderTeamSelector() {
+      var allTeams = Teams.all();
+      if (allTeams && allTeams.length > 1) {
+        return <div className="form-group">
+          <select className="form-control"
+                  value={this.state.selectedTeamId}
+                  onChange={this.changeTeam.bind(this)}>
+            {_.map(allTeams, (t) =>
+              <option key={t.teamid} value={t.teamid}>{t.team_name}</option>
+            )}
+          </select>
+        </div>;
+      }
+    }
+
+    changeTeam(event: Event) {
+      var target = event.target as HTMLOptionElement;
+      this.setState({selectedTeamId: target.value})
+    }
+
+    renderFooter() {
+      var dataStatus = Teams.dataStatus(this.state.selectedTeamId);
+      var busy = dataStatus === Model.DataStatus.INFLIGHT;
+
+      return <div className="clearfix modal-footer">
+        { busy ?
+          <div>
+            <div className="esper-spinner" />
+            <span className="esper-footer-text">
+              Saving &hellip; this may take a minute
+            </span>
+          </div> :
+          <button className="btn btn-secondary" onClick={this.props.onDone}>
+            Done
+          </button>
+        }
+      </div>;
+    }
+  }
+
+  export class CalAddModal extends Component<{}, {}> {
+    render() {
+      return <Modal title="Connect Calendars to Esper">
+        <CalAdd onDone={this.hideModal.bind(this)}/>
+      </Modal>;
+    }
+
+    hideModal() {
+      this.jQuery().modal('hide');
+    }
+  }
+}
