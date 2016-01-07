23e13f7fce39b36a1f9809e476d662bc70d839ae
Andrew Fong <andrewfong@esper.com>
2016-01-07 02:43:30 +0000

    Rewrite esper.com to use Nunjucks

--- esper.com/gulpfile.js
+++ esper.com/gulpfile.js
@@ -1,108 +1,102 @@
 "use strict";
 
-// For your configuration pleasure
-var config = require("./config");
-
 // NB: This gulp file is intended to be used with Gulp 4.x and won't
 // work with Gulp 3.x or below.
-var gulp = require("gulp"),
-    _ = require("lodash"),
-    cached = require("gulp-cached"),
-    ecstatic = require("ecstatic"),
-    del = require("del"),
-    filter = require("gulp-filter"),
-    http = require("http"),
-    less = require("gulp-less"),
-    livereload = require("livereload"),
-    path = require("path"),
-    rename = require("gulp-rename"),
-    sourcemaps = require("gulp-sourcemaps");
+var _ = require("lodash"),
+    gulp = require("gulp"),
+    helpers = require("../gulp-helpers"),
+    watch = helpers.watch(gulp);
 
-// NB: No caching for LESS since it could reference other LESS files
+/* Config vars */
+var config = {
+  htmlGlobs: ["html/**/*.html"],
+  htmlOut: "pub", // HTML goes to root rather than subdir
+
+  imgGlobs: ["img/**/*.*"],
+  imgOut: "pub/img",
+
+  jsGlobs: ["js/**/*.js"],
+  jsOut: "pub/js",
+
+  lessGlobs: ["css/**/*.less", "css/**/*.css"],
+  lessOut: "pub/css",
+
+  pubDir: "pub",
+
+  serverPort: 5000,
+  liveReloadPort: 35729
+};
+
+// This var gets set to true by production task
+var production = false;
+
+
+/* Gulp tasks */
+
 gulp.task("build-less", function() {
-  return gulp.src(path.join(config.lessDir, "**/*.less"))
-    .pipe(sourcemaps.init())
-    .pipe(less({
-      paths: [ path.join(__dirname, config.lessDir) ]
-    }))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(path.join(config.pubDir, "css")));
+  return helpers.less(config.lessGlobs, config.lessOut, {
+    production: production
+  });
 });
 
-// Make sure LESS compilation happens AFTER building CSS so that pre-compiled
-// CSS doesn't clobber our more recently compiled LESS files
-gulp.task("build-css", gulp.series(function() {
-  return gulp.src(path.join(config.cssDir, "**/*.css"))
-    .pipe(cached("css"))
-    .pipe(gulp.dest(path.join(config.pubDir, "css")));
-}, "build-less"));
+gulp.task("watch-less", watch(config.lessGlobs, "build-less"));
 
-// TODO: Uglify, concatenate, all that jazz
 gulp.task("build-js", function() {
-  return gulp.src(path.join(config.jsDir, "**/*.js"))
-    .pipe(cached("css"))
-    .pipe(gulp.dest(path.join(config.pubDir, "js")));
+  return helpers.js(config.jsGlobs, config.jsOut, {
+    production: production
+  });
 });
+
+gulp.task("watch-js", watch(config.jsGlobs, "build-js"));
 
 gulp.task("build-img", function() {
-  return gulp.src(path.join(config.imgDir, "**/*.*"))
-    .pipe(cached("img"))
-    .pipe(gulp.dest(path.join(config.pubDir, "img")));
+  return helpers.assets(config.imgGlobs, config.imgOut);
 });
+
+gulp.task("watch-img", watch(config.imgGlobs, "build-img"));
 
 gulp.task("build-html", function() {
-  // Get rid of extension, except for index.html
-  var indexFilter = filter(["*", "!index.html"], {restore: true});
-
-  return gulp.src(path.join(config.htmlDir, "**/*.html"))   
-    .pipe(cached("html")) 
-    .pipe(indexFilter)
-    .pipe(rename(function (path) {
-      path.extname = "";
-    }))
-    .pipe(indexFilter.restore)
-    .pipe(gulp.dest(config.pubDir));
-});
-
-// Launch a live-reload server that watches the public directory and sends
-// a websocket notification if things change. Use in conjunction with a
-// LiveReload browser extension. Uses port 35729 by default.
-gulp.task("live-reload", function() {
-  var server = livereload.createServer({
-    port: config.liveReloadPort
+  return helpers.html(config.htmlGlobs, config.htmlOut, {
+    production: production,
+    data: {
+      PRODUCTION: !!production
+    }
   });
-  server.watch(config.pubDir);
-  return server;
 });
 
-// Launch an HTML server that serves the contents in the pub dir with the 
-// correct content type
-gulp.task("html-server", function() {   
-  return http.createServer(
-    ecstatic({ root: path.join(__dirname, config.pubDir),
-               contentType: 'text/html' })
-  ).listen(config.serverPort);
+gulp.task("watch-html", watch(config.htmlGlobs, "build-html"));
+
+gulp.task("server", function(cb) {
+  return helpers.server(config.pubDir, cb, {
+    port: config.serverPort,
+    liveReloadPort: config.liveReloadPort
+  });
 });
 
-gulp.task("build", gulp.parallel("build-css", 
-                                 "build-html", 
+gulp.task("build", gulp.parallel("build-html",
                                  "build-js",
+                                 "build-less",
                                  "build-img"));
 
 gulp.task("clean", function(cb) {
-  del(config.pubDir, cb);
+  helpers.clean(config.pubDir, cb);
 });
 
+gulp.task("production", function(cb) {
+  production = true;
+  cb();
+});
+
+gulp.task("build-production", gulp.series("production", "clean", "build"))
+
 gulp.task("watch", gulp.series("clean", "build",
-  gulp.parallel("live-reload", "html-server", function() {
-    var dirs = _.map([config.htmlDir, 
-                      config.lessDir, 
-                      config.cssDir, 
-                      config.jsDir, 
-                      config.imgDir], function(dir) {
-      return path.join(dir, "**/*.*");
-    });
-    return gulp.watch(dirs, gulp.series("build"));
-})));
+  gulp.parallel(
+    "server",
+    "watch-html",
+    "watch-img",
+    "watch-js",
+    "watch-less"
+  )
+));
 
 gulp.task("default", gulp.series("build"));
---
+++ esper.com/js/landing.js
@@ -0,0 +1,43 @@
+function toggleNavMenu() {
+  if ($(".nav-menu").css("display") === "none") {
+    $(".nav-menu").fadeIn("fast");
+    $(document.body).css("overflow-y", "hidden");
+  } else {
+    $(".nav-menu").fadeOut("fast");
+    $(document.body).css("overflow-y", "auto");
+  }
+}
+
+function resizer() {
+  var splash = $(".splash");
+
+  window.addEventListener("resize", resizeCanvas, false);
+
+  function resizeCanvas() {
+    if (window.innerWidth < 800) {
+      var offset = Math.abs(window.innerWidth - 800)/2;
+      splash
+        .css("clip", "rect(0px," + (window.innerWidth + offset) + "px,400px,"
+          + offset + "px)")
+        .css("margin-left", -offset + "px");
+    } else {
+      splash
+        .css("clip", "rect(0," + window.innerWidth + "px,400px,0)")
+        .css("margin-left", 0);
+    }
+  }
+
+  resizeCanvas();
+}
+
+function loadElements() {
+  $(".nav-menu").click(function() { toggleNavMenu(); });
+  $(".nav-menu-toggle").click(function() { toggleNavMenu(); });
+}
+
+function main() {
+  loadElements();
+  resizer();
+}
+
+$(document).ready(main);
