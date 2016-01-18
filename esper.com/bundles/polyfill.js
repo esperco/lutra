bb9af01b69ee9a172a1a72f049d504b851eab7c0
Andrew Fong <andrewfong@esper.com>
2016-01-18 23:49:24 +0000

    Add location.origin as polyfill

--- esper.com/bundles/polyfill.js
+++ esper.com/bundles/polyfill.js
@@ -2,3 +2,7 @@
 window.requestAnimationFrame = window.requestAnimationFrame || function(cb) {
   setTimeout(cb, 0);
 }
+
+window.location.origin = window.location.origin || (
+  window.location.protocol + "//" + window.location.hostname +
+  (window.location.port ? ':' + window.location.port : ''));
