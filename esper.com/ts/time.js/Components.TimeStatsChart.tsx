4648b814d89203f5bfc484e5018467fcd1979796
Andrew Fong <andrewfong@esper.com>
2016-01-20 23:01:00 +0000

    Basic Highchart support

--- esper.com/ts/time.js/Components.TimeStatsChart.tsx
+++ esper.com/ts/time.js/Components.TimeStatsChart.tsx
@@ -17,5 +17,15 @@ module Esper.Components {
     displayResults?: TimeStats.DisplayResults;
   }
 
-  export class TimeStatsChart extends Component<TimeStatsChartProps, {}> {}
+  export class TimeStatsChart extends Component<TimeStatsChartProps, {}> {
+    renderNoData() {
+      return <div className="esper-expanded minus-subheader padded">
+        <div className="panel panel-default esper-focus-message">
+          <div className="panel-body">
+            No Data
+          </div>
+        </div>
+      </div>;
+    }
+  }
 }
