8f7be1a28d40ea5b4c2d43a9afc47e994662d636
Andrew Fong <andrewfong@esper.com>
2016-01-27 07:39:50 +0000

    Re-implement guest domains pie chart

--- esper.com/ts/time.js/Charts.GuestDomains.tsx
+++ esper.com/ts/time.js/Charts.GuestDomains.tsx
@@ -1,12 +1,11 @@
 /*
-  Bar chart for show label durations over time
+  Pie chart for showing guests by domain
 */
 
 /// <reference path="../lib/ReactHelpers.ts" />
 /// <reference path="./Charts.AutoChart.tsx" />
 /// <reference path="./Components.Highchart.tsx" />
 /// <reference path="./DailyStats.ts" />
-/// <reference path="./Calendars.ts" />
 /// <reference path="./Colors.ts" />
 
 module Esper.Charts {
@@ -31,84 +30,129 @@ module Esper.Charts {
 
     renderChart() {
       var data = this.sync()[0];
-      return <span />;
+      var domains = DailyStats.topGuestDomains(data);
 
-      // var totalTime = _.sum(data.top_guest_domains, (d) => d.split_time);
-      // var cutOffTime = TOP_GUESTS_CUT_OFF * totalTime;
+      // Some math and filtering with totals
+      var totalScheduled = DailyStats.sumScheduled(data);
+      var totalWithGuests = DailyStats.sumWithGuests(data);
+      var totalNoGuests = totalScheduled - totalWithGuests;
 
-      // var addressData: HighchartsDataPoint[] = [];
-      // var domainData: HighchartsDataPoint[] = _.map(data.top_guest_domains,
-      //   (d) => {
-      //     // For each domain, populate with guests for that domain
-      //     var domainGuests = _.filter(data.top_guests,
-      //       (g) => _.endsWith(g.id, '@' + d.id) && g.split_time > cutOffTime
-      //     );
-      //     domainGuests = _.sortBy(domainGuests, (g) => g.split_time);
+      var totalDomain = _.sum(domains, (d) => d.time);
+      var cutOffTime = TOP_GUESTS_CUT_OFF * totalDomain;
+      domains = _.filter(domains, (d) => d.time >= cutOffTime);
 
-      //     var baseColor = Colors.getColorForDomain(d.id);
-      //     var totalGuests = domainGuests.length;
-      //     _.each(domainGuests, (g, i) => {
-      //       addressData.push({
-      //         name: g.name ? `${g.name} <${g.id}>` : g.id,
-      //         color: Colors.lighten(baseColor,
-      //           ((totalGuests - i) / totalGuests) * MAX_COLOR_CHANGE),
-      //         y: TimeStats.toHours(g.split_time)
-      //       });
-      //     });
+      var totalNamedDomain = _.sum(domains, (d) => d.time)
+      var totalUnnamedDomain = totalDomain - totalNamedDomain;
 
-      //     var remainder = d.split_time -
-      //       _.sum(domainGuests, (g) => g.split_time);
-      //     if (remainder && remainder > 0) {
-      //       addressData.push({
-      //         name: "Other " + d.id,
-      //         color: baseColor,
-      //         y: TimeStats.toHours(remainder)
-      //       });
-      //     }
+      /////
 
-      //     return {
-      //       name: d.id,
-      //       color: baseColor,
-      //       y: TimeStats.toHours(d.split_time)
-      //     }
-      //   });
+      var addressData: HighchartsDataPoint[] = [];
+      var domainData: HighchartsDataPoint[] = _.map(domains,
+        (d) => {
+          // Filter from largest to smallest
+          var domainGuests = _.filter(d.guests,
+            (g) => g.time >= cutOffTime
+          );
 
-      // return <Components.Highchart opts={{
-      //   chart: {
-      //     type: 'pie'
-      //   },
+          // Each guest is colored a shade of base color for domain
+          var baseColor = Colors.getColorForDomain(d.domain);
+          var totalGuests = domainGuests.length;
+          _.each(domainGuests, (g, i) => {
+            addressData.push({
+              name: g.name ? `${g.name} (${g.email})` : g.email,
+              color: Colors.lighten(baseColor,
+                ((totalGuests - i) / totalGuests) * MAX_COLOR_CHANGE),
+              y: TimeStats.toHours(g.time)
+            });
+          });
 
-      //   series: [
-      //     {
-      //       size: '60%',
-      //       data: domainData,
-      //       dataLabels: {
-      //         enabled: true,
-      //         formatter: function () {
-      //           return this.percentage > 10 ? this.point.name : null;
-      //         },
-      //         color: '#ffffff',
-      //         style: { textShadow: "" },
-      //         backgroundColor: "#000000",
-      //         distance: -30
-      //       }
-      //     } as HighchartsPieChartSeriesOptions,
-      //     {
-      //       size: '80%',
-      //       innerSize: '60%',
-      //       data: addressData,
-      //       dataLabels: {
-      //         enabled: true,
-      //         formatter: function() {
-      //           if (this.percentage) {
-      //             return `${this.point.name} ` +
-      //               `(${this.percentage.toFixed(2)}%)`;
-      //           }
-      //         }
-      //       }
-      //     } as HighchartsPieChartSeriesOptions
-      //   ]
-      // }} units="Adjusted Hours" />;
+          // Guests less than cutoff get labeled as "Other"
+          var remainder = d.time - _.sum(domainGuests, (g) => g.time);
+          if (remainder && remainder > 0) {
+            addressData.push({
+              name: "Other " + d.domain,
+              color: baseColor,
+              y: TimeStats.toHours(remainder)
+            });
+          }
+
+          return {
+            name: d.domain,
+            color: baseColor,
+            y: TimeStats.toHours(d.time)
+          }
+        });
+
+        // Add "no guests"
+        if (totalNoGuests > 0) {
+          let color = Colors.lightGray;
+          let hours = TimeStats.toHours(totalNoGuests);
+          addressData.unshift({
+            name: "No Guests",
+            color: color,
+            y: hours
+          });
+
+          domainData.unshift({
+            name: "No Guests",
+            color: color,
+            y: hours
+          });
+        }
+
+        // Add "other"
+        if (totalUnnamedDomain > 0) {
+          var color = Colors.getColorForDomain("");
+          var hours = TimeStats.toHours(totalUnnamedDomain);
+          addressData.unshift({
+            name: "Other",
+            color: color,
+            y: hours
+          });
+
+          domainData.unshift({
+            name: "Other",
+            color: color,
+            y: hours
+          });
+        }
+
+      return <Components.Highchart opts={{
+        chart: {
+          type: 'pie'
+        },
+
+        series: [
+          {
+            size: '60%',
+            data: domainData,
+            dataLabels: {
+              enabled: true,
+              formatter: function () {
+                return this.percentage > 10 ? this.point.name : null;
+              },
+              color: '#ffffff',
+              style: { textShadow: "" },
+              backgroundColor: "#000000",
+              distance: -30
+            }
+          } as HighchartsPieChartSeriesOptions,
+          {
+            size: '80%',
+            innerSize: '60%',
+            data: addressData,
+            dataLabels: {
+              enabled: true,
+              formatter: function() {
+                if (this.percentage) {
+                  return `${this.point.name} ` +
+                    `(${this.percentage.toFixed(1)}%)`;
+                }
+              }
+            }
+          } as HighchartsPieChartSeriesOptions
+        ]
+      }} units="Adjusted Hours" />;
     }
   }
 }
