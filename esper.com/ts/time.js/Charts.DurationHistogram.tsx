04fc1c720cf33b40b19299cace3bc45186e38539
Andrew Fong <andrewfong@esper.com>
2016-03-02 22:22:19 +0000

    Bug fix for >= durations

--- esper.com/ts/time.js/Charts.DurationHistogram.tsx
+++ esper.com/ts/time.js/Charts.DurationHistogram.tsx
@@ -11,22 +11,22 @@ module Esper.Charts {
   // Durations for each bar in our histogram
   const DURATION_BUCKETS = [{
     label: "< 30m",
-    gt: 0   // Greater than, seconds
+    gte: 0   // Greater than, seconds
   }, {
     label: "30m +",
-    gt: 30 * 60
+    gte: 30 * 60
   }, {
     label: "1h +",
-    gt: 60 * 60
+    gte: 60 * 60
   }, {
     label: "2h +",
-    gt: 2 * 60 * 60
+    gte: 2 * 60 * 60
   }, {
     label: "4h +",
-    gt: 4 * 60 * 60
+    gte: 4 * 60 * 60
   }, {
     label: "8h +",
-    gt: 8 * 60 * 60
+    gte: 8 * 60 * 60
   }];
 
   export class DurationHistogram extends AutoChart {
@@ -40,7 +40,7 @@ module Esper.Charts {
       _.each(data.daily_stats, function(stat) {
         _.each(stat.scheduled, function(duration) {
           var i = _.findLastIndex(DURATION_BUCKETS, (b) => {
-            return duration > b.gt;
+            return duration >= b.gte;
           });
           aggDuration[i] += duration;
           frequency[i] += 1;
