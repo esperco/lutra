9391518131d82c75f615c099f320848d7b0740b5
Andrew F <andrewfong@esper.com>
2016-03-12 06:27:03 +0000

    Footer doesn't need links in mobile

--- esper.com/ts/time.js/Views.Footer.tsx
+++ esper.com/ts/time.js/Views.Footer.tsx
@@ -6,14 +6,21 @@ module Esper.Views {
 
   export class Footer extends ReactHelpers.Component<{}, {}> {
     renderWithData() {
+      var email = Option.cast(Login.InfoStore.val()).match({
+        none: () => "",
+        some: (info) => info.email
+      });
       return <div className="footer">
         <div className="container-fluid padded clearfix">
-          <ul className="footer-links pull-left nav nav-pills">
+          <ul className="footer-links pull-left nav nav-pills hidden-xs">
             <li><a href="/" target="_blank">Home</a></li>
             <li><a href="/contact" target="_blank">Contact</a></li>
             <li><a href="/privacy-policy" target="_blank">Privacy</a></li>
             <li><a href="/terms-of-use" target="_blank">Terms</a></li>
           </ul>
+          <div className="visible-xs-block navbar-text pull-left">
+            { email ? "Logged in as " + email : "" }
+          </div>
           <div className="pull-right navbar-text"
                onDoubleClick={this.testError}>
             &copy; {new Date().getFullYear()} Esper
