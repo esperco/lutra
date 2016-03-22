41e9844daef03b2c73ffecc6fd155df008ced707
Andrew F <andrewfong@esper.com>
2016-03-22 23:59:13 +0000

    Batch fetching for Model2

---
+++ lib-ts/Model2.Batch.ts
@@ -0,0 +1,86 @@
+/// <reference path="./Model2.ts" />
+
+module Esper.Model2 {
+
+  // Interface used for passsing batch data to BatchStore
+  export interface BatchVal<ItemKey, ItemData> {
+    itemKey: ItemKey,
+    data: Option.T<ItemData>,
+    opts?: Model2.StoreOpts<ItemKey>
+  }
+
+  export class BatchStore<BatchKey, ItemKey, ItemData>
+    extends Store<BatchKey, ItemKey[]>
+  {
+    itemStore: Store<ItemKey, ItemData>;
+
+    constructor(itemStore: Store<ItemKey, ItemData>, opts?: {
+      // Max number of keys to store
+      cap?: number
+    }) {
+      super(opts);
+      this.itemStore = itemStore;
+    }
+
+    batchSet(batchKey: BatchKey,
+             batchVals: BatchVal<ItemKey, ItemData>[],
+             opts?: Model2.StoreOpts<BatchKey>) {
+      var keys: ItemKey[] = [];
+
+      this.transact(() => {
+        this.itemStore.transact(() => {
+          _.each(batchVals, (batchVal) => {
+            if (batchVal.opts) {
+              this.itemStore.set(batchVal.itemKey, batchVal.data,
+                                 batchVal.opts);
+            } else {
+              this.itemStore.set(batchVal.itemKey, batchVal.data);
+            }
+            keys.push(batchVal.itemKey);
+          });
+
+          if (opts) {
+            this.set(batchKey, Option.wrap(keys), opts);
+          } else {
+            this.set(batchKey, Option.wrap(keys));
+          }
+        });
+      });
+    }
+
+    batchGet(batchKey: BatchKey) {
+      return this.get(batchKey)
+        .flatMap((x) => x.data)
+        .flatMap((d) => Option.wrap(_.map(d,
+          (k) => this.itemStore.get(k)
+        )))
+        .flatMap((l) => _.find(l, (i) => i.isNone()) ?
+          Option.none<typeof l>() :
+          Option.wrap(l)
+        );
+    }
+
+    batchFetch(batchKey: BatchKey,
+               promise: JQueryPromise<Option.T<BatchVal<ItemKey, ItemData>[]>>)
+    {
+      var batchPromise = promise.then(function(optBatchVals) {
+        return optBatchVals.flatMap((vals) => vals.length ?
+          Option.wrap(_.map(vals, (v) => v.itemKey)) :
+          Option.none<ItemKey[]>());
+      });
+
+      // This updates batch store
+      this.fetch(batchKey, batchPromise);
+
+      // Update items stores too. Use the itemStore's fetch rather than
+      // upsert directly so we can take advantage of dataStatus handling code
+      promise.done((optBatchVals) => optBatchVals.match({
+        none: () => null, // Ignore if no values returned
+        some: (batchVals) => this.itemStore.transact(() =>
+          _.each(batchVals, (val) => this.itemStore.fetch(
+            val.itemKey,
+            $.Deferred().resolve(val.data).promise())))
+      }));
+    }
+  }
+}
