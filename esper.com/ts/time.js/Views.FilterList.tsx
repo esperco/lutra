3eb0f529f58d6cba204a5513bb77ce0eb0035255
Andrew F <andrewfong@esper.com>
2016-04-05 05:37:23 +0000

    Refactor event list into its own component

--- esper.com/ts/time.js/Views.FilterList.tsx
+++ esper.com/ts/time.js/Views.FilterList.tsx
@@ -315,107 +315,20 @@ module Esper.Views {
     }
 
     renderMain(events: Events2.TeamEvent[]) {
-      if (events.length === 0) {
-        return <div className="esper-no-content">
-          No events found
-        </div>;
-      }
-
-      var groupByDays = _.groupBy(events,
-        (e) => moment(e.start).clone().startOf('day').valueOf()
-      );
-      var sortedKeys = _.sortBy(_.keys(groupByDays), (k) => parseInt(k));
-
-      return <div>
-        {
-          _.map(sortedKeys, (k) =>
-            this.renderDay(parseInt(k), groupByDays[k])
-          )
-        }
-      </div>;
-    }
-
-    renderDay(timestamp: number, events: Events2.TeamEvent[]) {
-      var m = moment(timestamp);
-      return <div className="day" key={timestamp}>
-        <div className="day-title">{ m.format("MMM D - dddd") }</div>
-        <div className="list-group">
-          { _.map(events, (e) => this.renderEvent(e)) }
-        </div>
-      </div>
-    }
-
-    renderEvent(event: Events2.TeamEvent) {
-      return <div key={[event.teamId, event.calendar_id, event.id].join(",")}
-                  className="list-group-item event">
-        <div className="event-checkbox"
-             onClick={() => this.toggleEvent(event)}>
-          { this.isSelected(event) ?
-            <i className="fa fa-fw fa-check-square-o" /> :
-            <i className="fa fa-fw fa-square-o" />
+      return <Components.EventList
+        events={events}
+        selectedEvents={this.state.selected}
+        onEventClick={(event) => this.editEvent(event)}
+        onEventToggle={(event) => this.toggleEvent(event)}
+        onLabelClick={(event, id) => this.updateRoute({
+          labels: {
+            some: [id],
+            all: false,
+            none: false,
+            unmatched: false
           }
-        </div>
-        <div className="event-content">
-          <div className={"title esper-link" +
-                 (event.feedback.attended === false ? " no-attend" : "")}
-               onClick={() => this.editEvent(event)}>
-            {event.title}
-          </div>
-          <div className="time">
-            <span className="start">
-              { moment(event.start).format("h:mm a") }
-            </span>{" to "}<span className="end">
-              { moment(event.end).format("h:mm a") }
-            </span>{" "}
-            { event.recurring_event_id ?
-              <span className="recurring" title="Recurring">
-                <i className="fa fa-fw fa-refresh" />
-              </span> :
-              null
-            }
-          </div>
-          <div className="event-rating">
-            { _.times((event.feedback.attended !== false &&
-                       event.feedback.rating) || 0, (i) =>
-              <i key={i.toString()} className="fa fa-fw fa-star" />
-            )}
-          </div>
-          <div className="event-labels">
-            { _.map(event.labels_norm,
-              (l, i) => this.renderLabel(event, l, event.labels[i])
-            ) }
-          </div>
-        </div>
-      </div>;
-    }
-
-    renderLabel(event: Events2.TeamEvent, id: string, displayAs: string) {
-      var labelColor = Colors.getColorForLabel(id)
-      var style = {
-        background: labelColor,
-        color: Colors.colorForText(labelColor)
-      };
-
-      var onClick = () => updateRoute(_.extend({}, this.props, {
-        labels: [id],
-        unlabeled: false,
-        allLabels: false
-      }) as FilterListProps);
-      var clearLabel = ((e: __React.MouseEvent) => {
-        e.stopPropagation();
-        EventLabelChange.remove([event], displayAs);
-      });
-
-      return <span style={style} key={id} className="event-label"
-        onClick={onClick}
-      >
-        <i className="fa fa-fw fa-tag" />{" "}
-        {displayAs}
-        <span className="hidden-xs esper-clear-action"
-              onClick={(e) => clearLabel(e)}>{" "}
-          <i className="fa fa-fw fa-times" />
-        </span>
-      </span>;
+        })}
+      />;
     }
 
     editEvent(event: Events2.TeamEvent) {
