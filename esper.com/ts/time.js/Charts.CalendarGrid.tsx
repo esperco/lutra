54d6622c4cceffeb04511415a6472c05c1220a44
Andrew F <andrewfong@esper.com>
2016-04-05 17:36:09 +0000

    Combine calendar grid charts

--- esper.com/ts/time.js/Charts.CalendarGrid.tsx
+++ esper.com/ts/time.js/Charts.CalendarGrid.tsx
@@ -1,4 +1,3 @@
-/// <reference path="./Charts.AutoChart.tsx" />
 /// <reference path="./Components.CalendarGrid.tsx" />
 /// <reference path="./Components.MonthSelector.tsx" />
 /// <reference path="./Esper.ts" />
@@ -9,28 +8,66 @@ module Esper.Charts {
   /*
     Base class for calendar grid-style Autochart
   */
-  export abstract class CalendarGridChart extends AutoChart<Charts.ChartJSON> {
+  export abstract class CalendarGridChart extends EventChart<{}> {
+    protected eventsForDates: {
+      date: Date;
+      durations: EventStats.HasDurations[];
+    }[];
+
+    sync() {
+      super.sync();
+
+      var bounds = Period.boundsFromPeriod(this.params.period);
+      var dates = Events2.datesFromBounds(bounds[0], bounds[1]);
+      this.eventsForDates = _.map(dates, (d) => {
+        var events: Events2.TeamEvent[] = [];
+        _.each(this.params.cals, (cal) => {
+          Events2.EventsForDateStore.batchGet({
+            teamId: cal.teamId,
+            calId: cal.calId,
+            date: d
+          }).match({
+            none: () => null,
+            some: (s) => s.data.match({
+              none: () => null,
+              some: (optList) => _.each(optList, (opt) => opt.data.match({
+                none: () => null,
+                some: (event) => events.push(event)
+              }))
+            })
+          });
+        });
+
+        events = this.filterEvents(events, this.params.filterParams);
+        var wrappers = _.map(events, (e) => ({ event: e }));
+        var durations = EventStats.getDurations(wrappers, {
+          truncateStart: moment(d).startOf('day').toDate(),
+          truncateEnd: moment(d).endOf('day').toDate(),
+        });
+
+        return {
+          date: d,
+          durations: durations
+        };
+      });
+    }
+
     renderChart() {
+      var start = Period.boundsFromPeriod(this.params.period)[0]
       return <Components.CalendarGrid
-        date={new Date(this.params.start)}
+        date={start}
         dayFn={this.dayFn.bind(this)}
       />;
     }
 
     noData() {
-      // Always show grid if there's an API response, even if empty
-      return !this.sync()[0];
+      return false;
     }
 
     protected abstract dayFn(m: moment.Moment): JSX.Element;
 
-    // Use a simple month selector rather than allow custom periods
-    renderPeriodSelector() {
-      return <Components.MonthSelector
-        windowStart={new Date(this.params.start)}
-        windowEnd={new Date(this.params.end)}
-        updateFn={(x) => this.updatePeriod(x)}
-      />;
+    intervalsAllowed(): Period.Interval[] {
+      return ["month"];
     }
   }
 }
