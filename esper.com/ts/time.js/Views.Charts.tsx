7330c06843c0091cf5bc82247ba072f4ce87e04c
Andrew F <andrewfong@esper.com>
2016-04-07 23:46:41 +0000

    Plural

--- esper.com/ts/time.js/Views.Charts.tsx
+++ esper.com/ts/time.js/Views.Charts.tsx
@@ -56,7 +56,7 @@ module Esper.Views {
           <div className="esper-menu-section">
             <label htmlFor={this.getId("cal-select")}>
               <i className="fa fa-fw fa-calendar-o" />{" "}
-              Calendar
+              Calendars
             </label>
             <Components.CalSelectorDropdown
               id={this.getId("cal-select")}
