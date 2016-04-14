f0d0485478acef3d19db755b134636d08c0df174
Andrew F <andrewfong@esper.com>
2016-04-14 00:45:01 +0000

    Custom selector component with caret

---
+++ esper.com/ts/common/Components.SelectMenu.tsx
@@ -0,0 +1,45 @@
+/*
+  Drop in (more or less) replacement for <select> element using DropdownModal
+  and Selector components
+*/
+
+/// <reference path="./Components.DropdownModal.tsx" />
+/// <reference path="./Components.Selector.tsx" />
+
+module Esper.Components {
+  interface Props {
+    options: {
+      val: string;
+      display: JSX.Element|string;
+    }[];
+    onChange: (s: string) => void;
+
+    id?: string;
+    placeholder?: string;
+    selected?: string;
+  }
+
+  export class SelectMenu extends ReactHelpers.Component<Props ,{}> {
+    render() {
+      var displayText: JSX.Element|string = this.props.placeholder || "";
+      if (this.props.selected) {
+        var selectedOpt = _.find(this.props.options,
+          (o) => o.val === this.props.selected
+        );
+        if (selectedOpt) {
+          displayText = selectedOpt.display;
+        }
+      }
+      return <Components.DropdownModal>
+        <Components.Selector id={this.props.id} className="dropdown-toggle">
+          { displayText }
+        </Components.Selector>
+        <ul className="dropdown-menu">
+          { _.map(this.props.options, (o) => <li key={o.val}>
+            <a onClick={() => this.props.onChange(o.val)}>{ o.display }</a>
+          </li>)}
+        </ul>
+      </Components.DropdownModal>
+    }
+  }
+}
---
+++ esper.com/ts/common/Components.Selector.tsx
@@ -0,0 +1,31 @@
+/*
+  Simple select menu (this is only the display value, use Dropdown or
+  DropdownModal for the actual menu itself).
+*/
+
+/// <reference path="../lib/ReactHelpers.ts" />
+
+module Esper.Components {
+  export class Selector extends ReactHelpers.Component<{
+    id?: string;
+    className?: string;
+    children?: JSX.Element[];
+    onClick?: (e: React.MouseEvent) => void;
+  }, {}> {
+    render() {
+      var classes = this.props.className +
+        " form-control esper-selector clearfix";
+      return <div className={classes} onClick={this.props.onClick}>
+
+        {/* Tiny input box so label with 'for' property can trigger onClick */}
+        <input type="text" id={this.props.id}
+               style={{visibility: 'hidden', height: '1px', width: '1px'}} />
+
+        <div className="esper-selector-content">
+          <i className="fa fa-fw fa-caret-down pull-right" />
+          {this.props.children}
+        </div>
+      </div>
+    }
+  }
+}
