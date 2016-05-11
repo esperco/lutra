5fc7fd7634d12a6b89161b31bc498daa1f10ce10
Andrew F <andrewfong@esper.com>
2016-05-11 21:19:26 +0000

    Move lib references to single file that gets included at start

---
+++ esper.com/ts/time.js/_lib.ts
@@ -0,0 +1,28 @@
+/*
+  Stick references to lib here -- makes managing all the refs easier
+*/
+
+/// <reference path="./Esper.ts" />
+/// <reference path="../lib/Actions.Teams.ts" />
+
+/// <reference path="../lib/Analytics.Web.ts" />
+/// <reference path="../lib/Api.ts" />
+/// <reference path="../lib/Colors.ts" />
+/// <reference path="../lib/Layout.tsx" />
+/// <reference path="../lib/Model.Capped.ts" />
+/// <reference path="../lib/Option.ts" />
+/// <reference path="../lib/Period.ts" />
+/// <reference path="../lib/ReactHelpers.ts" />
+/// <reference path="../lib/Stores.Calendars.ts" />
+/// <reference path="../lib/Stores.Events.ts" />
+/// <reference path="../lib/Stores.Teams.ts" />
+/// <reference path="../lib/Queue.ts" />
+
+/// <reference path="../lib/Components.Dropdown.tsx" />
+/// <reference path="../lib/Components.DropdownModal.tsx" />
+/// <reference path="../lib/Components.ErrorMsg.tsx" />
+/// <reference path="../lib/Components.Modal.tsx" />
+/// <reference path="../lib/Components.ModalPanel.tsx" />
+/// <reference path="../lib/Components.Selector.tsx" />
+/// <reference path="../lib/Components.SignalStrength.tsx" />
+/// <reference path="../lib/Components.Tooltip.tsx" />
