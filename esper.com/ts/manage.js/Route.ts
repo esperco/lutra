1a8e1f4d2cc8951eee457bbf84698d5ae8206f77
Andrew F <andrewfong@esper.com>
2016-05-19 21:22:21 +0000

    Don't launch team settings page if no team

--- esper.com/ts/manage.js/Route.ts
+++ esper.com/ts/manage.js/Route.ts
@@ -5,6 +5,14 @@
 module Esper.Route {
   declare var pageJs: PageJS.Static;
 
+  var onboardingCheck: PageJS.Callback = function(ctx, next) {
+    if (Stores.Teams.all().length) {
+      next();
+    } else {
+      Route.nav.go(Paths.Manage.newTeam())
+    }
+  }
+
   routeHome(
     redirectPath(Paths.Manage.general())
   );
@@ -13,21 +21,25 @@ module Esper.Route {
     Actions.renderNewTeam();
   });
 
-  route(Paths.Manage.general({teamId: ":teamId?"}).hash, function(ctx) {
-    Actions.renderGeneralSettings(ctx.params["teamId"]);
-  });
+  route(Paths.Manage.general({teamId: ":teamId?"}).hash, onboardingCheck,
+    function(ctx) {
+      Actions.renderGeneralSettings(ctx.params["teamId"]);
+    });
 
-  route(Paths.Manage.calendars({teamId: ":teamId?"}).hash, function(ctx) {
-    Actions.renderCalendarSettings(ctx.params["teamId"]);
-  });
+  route(Paths.Manage.calendars({teamId: ":teamId?"}).hash, onboardingCheck,
+    function(ctx) {
+      Actions.renderCalendarSettings(ctx.params["teamId"]);
+    });
 
-  route(Paths.Manage.labels({teamId: ":teamId?"}).hash, function(ctx) {
-    Actions.renderLabelSettings(ctx.params["teamId"]);
-  });
+  route(Paths.Manage.labels({teamId: ":teamId?"}).hash, onboardingCheck,
+    function(ctx) {
+      Actions.renderLabelSettings(ctx.params["teamId"]);
+    });
 
-  route(Paths.Manage.notifications({teamId: ":teamId?"}).hash, function(ctx) {
-    Actions.renderNotificationSettings(ctx.params["teamId"]);
-  });
+  route(Paths.Manage.notifications({teamId: ":teamId?"}).hash, onboardingCheck,
+    function(ctx) {
+      Actions.renderNotificationSettings(ctx.params["teamId"]);
+    });
 
   routeNotFound(function(ctx) {
     Actions.render(React.createElement(Views.NotFound, {}));
