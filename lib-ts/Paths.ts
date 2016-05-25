4d7aa28c50e74c12d009c16613dbf765f0f54366
Keith Yeung <kungfukeith11@gmail.com>
2016-05-25 17:05:29 -0400

    Add basic infrastructure for rendering New Group page

--- lib-ts/Paths.ts
+++ lib-ts/Paths.ts
@@ -133,6 +133,10 @@ module Esper.Paths {
       return optPath(prefix, "new-team");
     }
 
+    export function newGroup() {
+      return optPath(prefix, "new-group");
+    }
+
     export module Team {
       export const subprefix = "team/";
 
@@ -162,7 +166,7 @@ module Esper.Paths {
       return optPath(prefix, "");
     }
 
-    export function list() {
+    export function list({groupId} : {groupId?: string} = {}) {
       return optPath(prefix, "list");
     }
   }
