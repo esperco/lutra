0063c959e7c9fc7a23d5083517b305d3b3237fbe
Andrew F <andrewfong@esper.com>
2016-06-01 23:04:27 +0000

    Don't capitalize all tooltips

--- lib-ts/Text.ts
+++ lib-ts/Text.ts
@@ -23,8 +23,8 @@ module Esper.Text {
     return `We are ${Util.roundStr(score * 100, 0)}% confident that this ` +
            `${Label.toLowerCase()} is applicable to this event`;
   }
-  export const EditLabels = "Edit " + Labels;
-  export const ConfirmLabels = "Confirm " + Labels;
+  export const EditLabels = "Edit " + _.capitalize(Labels);
+  export const ConfirmLabels = "Confirm " + _.capitalize(Labels);
 
   // Calendar List
   export const NoCalendarError =
