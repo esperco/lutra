573f3dafde5302a2c8ed643cbf01fce85dbb724e
Andrew F <andrewfong@esper.com>
2016-06-20 18:43:36 +0000

    Duration calculations incl. overlapping events

--- esper.com/ts/time.js/EventStats2.ts
+++ esper.com/ts/time.js/EventStats2.ts
@@ -41,8 +41,10 @@ module Esper.EventStats {
     }
   }
 
-  // Convert annotations to grouping -- optionally takes an existing grouping
-  // to add to
+  /*
+    Convert annotations to grouping -- optionally takes an existing grouping
+    to add to
+  */
   export function groupAnnotations(annotations: Annotation[],
                                    grouping?: OptGrouping) {
     grouping = grouping || {
@@ -77,17 +79,104 @@ module Esper.EventStats {
   }
 
 
+  /*
+    Calculate durations of events after adjusting for overlapping
+  */
+  interface DurationWrapper {
+    event: Stores.Events.TeamEvent;
+    duration: number;
+  }
+
+  export function durationWrappers(events: Stores.Events.TeamEvent[],
+    opts: DurationOpts = {}): DurationWrapper[]
+  {
+    // Make sure events are sorted by start date
+    events = _.sortBy(events, (e) => moment(e.start).valueOf());
+
+    // Construct initial annotations
+    var ret: DurationWrapper[] = _.map(events, (e) => ({
+      event: e,
+      duration: 0
+    }));
+
+    /* Overlap, truncate calculation */
+
+    // Use strings for maps because numbers convert to Array
+    var startMap: {[index: string]: DurationWrapper[]} = {};
+    var endMap: {[index: string]: DurationWrapper[]} = {};
+
+    // Critical points = start / end points
+    var criticalPoints: number[] = [];
+
+    _.each(ret, (e) => {
+
+      // Truncate start
+      var event = e.event;
+      var startM = moment(event.start);
+      if (opts.truncateStart && moment(opts.truncateStart).diff(startM) > 0) {
+        startM = moment(opts.truncateStart);
+      }
+      var start = startM.valueOf();
+
+      // Truncate end
+      var endM = moment(event.end);
+      if (opts.truncateEnd && moment(opts.truncateEnd).diff(endM) < 0) {
+        endM = moment(opts.truncateEnd);
+      }
+      var end = endM.valueOf();
+
+      // Ignore invalid start/end dates (possible after truncation)
+      if (end <= start) { return; }
+
+      // Map critical points to check for overlaps
+      var startStr = start.toString();
+      var endStr = end.toString();
+      startMap[startStr] = startMap[startStr] || [];
+      startMap[startStr].push(e);
+      endMap[endStr] = endMap[endStr] || [];
+      endMap[endStr].push(e);
+      criticalPoints.push(start);
+      criticalPoints.push(end);
+    });
+
+    criticalPoints.sort();
+    var criticalPoints = _.sortedUniq(criticalPoints);
+
+    // Stack of active events as we move through time
+    var eventStack: DurationWrapper[] = [];
+
+    _.each(criticalPoints, (p, i) => {
+      // Add and remove from current stack
+      var str = p.toString();
+      eventStack = eventStack.concat(startMap[str] || []);
+      _.each(endMap[str] || [], (e) => {
+        eventStack = _.without(eventStack, e)
+      });
+
+      // Add time to each event in the stack
+      var next = criticalPoints[i + 1];
+      if (next) {
+        var incr = ((next - p) / eventStack.length) / 1000;
+        _.each(eventStack, (e) => e.duration += incr);
+      }
+    });
+
+    return ret;
+  }
+
+
+  /////
+
   /* Settings for Calculation */
 
   // How many events to annotate or group at any given time
   const DEFAULT_MAX_PROCESS_EVENTS = 10;
 
-
   /*
     Asynchronous, non-blocking annotation and grouping of a series of events,
     with emission of change event at end of calculation.
   */
-  export abstract class Calculation extends Emit.EmitBase {
+  export abstract class CalculationBase extends Emit.EmitBase {
     // Are we there yet?
     ready = false;
     running = false;
@@ -166,8 +255,29 @@ module Esper.EventStats {
       window.requestAnimationFrame(this.runLoop);
     }
 
-    // Annotate some events from queue, returns true if it did work, false
-    // if queue was empty
+    // Annotate some events from queue
+    abstract annotateSome(): void;
+
+    groupSome() {
+      // Get events to process
+      var annotations = this.annotationsQueue.slice(0,
+        this.MAX_PROCESS_EVENTS);
+
+      // Do some grouping
+      groupAnnotations(annotations, this.grouping);
+
+      // Removed processed items from queue
+      this.annotationsQueue = this.annotationsQueue.slice(
+        this.MAX_PROCESS_EVENTS);
+    }
+  }
+
+
+  /*
+    Standard calculation => just process fixed number of events
+  */
+  export abstract class Calculation extends CalculationBase {
+    // Annotate some events from queue
     annotateSome() {
       // Get events to process
       var events = this.eventQueue.slice(0, this.MAX_PROCESS_EVENTS);
@@ -183,23 +293,71 @@ module Esper.EventStats {
       });
 
       // Remove processed items from queue
-      this.eventQueue = this.eventQueue.slice(this.MAX_PROCESS_EVENTS);
-    }
-
-    groupSome() {
-      // Get events to process
-      var annotations = this.annotationsQueue.slice(0,
-        this.MAX_PROCESS_EVENTS);
-
-      // Do some grouping
-      groupAnnotations(annotations, this.grouping);
-
-      // Removed processed items from queue
-      this.annotationsQueue = this.annotationsQueue.slice(
-        this.MAX_PROCESS_EVENTS);
+      this.eventQueue = this.eventQueue.slice(events.length);
     }
 
     // Replace with per-chart annotations
     abstract annotate(event: Stores.Events.TeamEvent): Annotation|Annotation[];
+  }
+
+
+  /*
+    Variant of calculation for doing duration-based calculations. When looking
+    at a single batch of events for duration calc purposes, we need to consider
+    any overlapping events as well (since we split time between overlapping
+    events).
+
+    MAX_PROCESS_EVENTS in this case is treated as a suggestion, rather than
+    a hard rule.
+  */
+  export abstract class DurationCalculation extends CalculationBase {
+    getSomeEvents() {
+      var events = this.eventQueue.slice(0, this.MAX_PROCESS_EVENTS);
+      let ends = _.map(events, (e) => e.end.getTime());
+      let max = _.max(ends);
+
+      var i = this.MAX_PROCESS_EVENTS;
+      var next = this.eventQueue[i];
+      while (!!next) {
+        if (next.start.getTime() < max) {
+          events.push(next);
+          max = Math.max(max, next.end.getTime());
+          i += 1;
+          next = this.eventQueue[i];
+        }
+        else {
+          break;
+        }
+      }
+
+      return events;
+    }
+
+    // Annotate some events from queue, returns true if it did work, false
+    // if queue was empty
+    annotateSome() {
+      // Get events to process
+      var events = this.getSomeEvents();
+
+      // Get durations
+      var durations = durationWrappers(events);
+
+      // Actual annotations
+      _.each(durations, (d) => {
+        let annotation = this.annotate(d.event, d.duration);
+        if (_.isArray(annotation)) {
+          this.annotationsQueue = this.annotationsQueue.concat(annotation);
+        } else {
+          this.annotationsQueue.push(annotation);
+        }
+      });
+
+      // Remove processed items from queue
+      this.eventQueue = this.eventQueue.slice(events.length);
+    }
+
+    // Replace with per-chart annotations
+    abstract annotate(event: Stores.Events.TeamEvent, duration: number)
+      : Annotation|Annotation[];
   }
 }
