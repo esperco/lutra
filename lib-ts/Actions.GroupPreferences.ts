528a9c6fa65bcfd98ec2afbd509e5822115cca8e
Keith Yeung <kungfukeith11@gmail.com>
2016-06-29 23:31:09 +0800

    Add section to group notifications for setting up bad meeting notifications

--- lib-ts/Actions.GroupPreferences.ts
+++ lib-ts/Actions.GroupPreferences.ts
@@ -51,4 +51,42 @@ module Esper.Actions.GroupPreferences {
       return prefs;
     });
   }
+
+  // Enable or disable bad meeting notifications
+  export function toggleBadMeetingNotifications(oldPrefs: ApiT.GroupPreferences) {
+    var groupId = oldPrefs.groupid;
+    var newPrefs = _.cloneDeep(oldPrefs);
+    newPrefs.bad_meeting_warning = !oldPrefs.bad_meeting_warning;
+    var promise = Api.putGroupPreferences(groupId, newPrefs);
+    update(groupId, promise, (prefs) => {
+      prefs.bad_meeting_warning = newPrefs.bad_meeting_warning;
+      return prefs;
+    });
+  }
+
+  // Sets the bad meeting duration for bad meeting notifications
+  export function setBadMeetingDuration(oldPrefs: ApiT.GroupPreferences,
+                                        duration: number) {
+    var groupId = oldPrefs.groupid;
+    var newPrefs = _.cloneDeep(oldPrefs);
+    newPrefs.bad_duration = duration;
+    var promise = Api.putGroupPreferences(groupId, newPrefs);
+    update(groupId, promise, (prefs) => {
+      prefs.bad_duration = duration;
+      return prefs;
+    });
+  }
+
+  // Sets the bad meeting attendees for bad meeting notifications
+  export function setBadMeetingAttendees(oldPrefs: ApiT.GroupPreferences,
+                                         attendees: number) {
+    var groupId = oldPrefs.groupid;
+    var newPrefs = _.cloneDeep(oldPrefs);
+    newPrefs.bad_attendees = attendees;
+    var promise = Api.putGroupPreferences(groupId, newPrefs);
+    update(groupId, promise, (prefs) => {
+      prefs.bad_attendees = attendees;
+      return prefs;
+    });
+  }
 }
