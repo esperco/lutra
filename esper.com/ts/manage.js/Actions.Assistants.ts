551a0bf69744f84aa015f2479b449154d3d4cb38
Andrew F <andrewfong@esper.com>
2016-07-06 21:21:44 +0000

    Allow removal of team owner, cal user

--- esper.com/ts/manage.js/Actions.Assistants.ts
+++ esper.com/ts/manage.js/Actions.Assistants.ts
@@ -15,10 +15,66 @@ module Esper.Actions.Assistants {
     var team = Stores.Teams.require(teamId);
     if (! team) return;
 
-    team = _.cloneDeep(team)
+    team = _.cloneDeep(team);
     _.pull(team.team_assistants, uid);
 
-    var p = Api.removeAssistant(teamId, uid);
+    var newOwnerId = getNewOwnerId(team, uid);
+    if (!newOwnerId && (team.team_owner === uid ||
+                        team.team_cal_user === uid))
+    {
+      // Unable to fill replacement owner -> treat as team deactivation instead
+      return Actions.Teams.removeTeam(teamId);
+    }
+
+    var p = (function() {
+
+      // Cannot remove team executive
+      if (team.team_executive === uid) {
+        var errMsg = `Cannot remove team exec - ${teamId} - ${uid}`;
+        Log.e(errMsg);
+        return $.Deferred<void[]>().reject(errMsg).promise();
+      }
+
+      // Removing team owner => try to change
+      var promises: JQueryPromise<void>[] = [];
+      if (team.team_owner === uid) {
+        team.team_owner = newOwnerId;
+        promises.push(Api.setTeamOwner(teamId, newOwnerId));
+      }
+
+      // Remove calendar user => try to change
+      if (team.team_cal_user === uid) {
+        team.team_cal_user = newOwnerId;
+        promises.push(Api.setTeamCalUser(teamId, newOwnerId));
+      }
+
+      return Util.when(promises);
+    })().then(function() {
+      return Api.removeAssistant(teamId, uid);
+    });
+
     Stores.Teams.TeamStore.push(teamId, p, Option.some(team));
   }
+
+  function getNewOwnerId(team: ApiT.Team, uidToRemove: string) {
+    if (team.team_approved && uidToRemove !== team.team_executive) {
+      return team.team_executive;
+    }
+
+    /*
+      NB: Currently not possible to remove self, but we might allow this
+      in future
+    */
+    else if (uidToRemove !== Login.myUid()) {
+      return Login.myUid();
+    }
+
+    // Crapshoot. Return first assistant that isn't being removed.
+    else {
+      return _.find(team.team_assistants,
+        (a) => a !== uidToRemove && a !== team.team_executive
+      );
+    }
+  }
 }
+
