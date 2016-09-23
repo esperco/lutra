b26c2142d251f558c665f996b4ea66753f2fc821
Andrew F <andrewfong@esper.com>
2016-09-23 19:51:10 +0000

    New generic chart component that uses ChartProps and GroupBy properties instead of sub-classing.

---
+++ esper.com/ts/time.js/Components.BarChart.tsx
@@ -0,0 +1,63 @@
+/*
+  Base class for a chart that shows absolute values as a bar chart -- takes
+  the form of a stacked bar with each event equal to one stack segement
+*/
+module Esper.Components {
+  export function BarChart({
+    series, categories, simplified, orientation, yAxis
+  } : {
+    series: Charting.EventSeries[],
+    categories: string[];
+    simplified?: boolean;
+    orientation?: 'vertical'|'horizontal'
+    yAxis?: string;
+  }) {
+    orientation = orientation || 'horizontal';
+
+    return <Highchart showExport={!simplified} opts={{
+      chart: orientation === 'vertical' ? {
+        type: 'column'
+      } : {
+        type: 'bar',
+        height: simplified ? series.length * 50 : series.length * 50 + 120
+      },
+
+      tooltip: Charting.eventPointTooltip,
+
+      legend: {
+        enabled: false
+      },
+
+      plotOptions: {
+        column: {
+          stacking: 'normal'
+        },
+
+        bar: {
+          borderWidth: 1,
+          stacking: 'normal'
+        }
+      },
+
+      xAxis: {
+        categories: categories
+      },
+
+      yAxis: [{
+        title: yAxis && !simplified ? { text: yAxis } : null,
+        visible: !simplified,
+        stackLabels: {
+          enabled: !simplified,
+          formatter: Charting.stackPointFormatter,
+          style: {
+            fontSize: "10px",
+            fontWeight: "normal",
+            textShadow: "none"
+          }
+        }
+      }],
+
+      series: series
+    }} />;
+  }
+}
---
+++ esper.com/ts/time.js/Components.PieChart.tsx
@@ -0,0 +1,50 @@
+/*
+  Base class for pie chart
+*/
+module Esper.Components {
+  export function PieChart({series, simplified, yAxis} : {
+    series: Charting.SingleEventGroupSeries;
+    simplified?: boolean;
+    yAxis?: string;
+  }) {
+    return <Highchart showExport={!simplified} opts={{
+      chart: {
+        type: 'pie'
+      },
+
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            formatter: function() {
+              if (this.percentage) {
+                if (simplified && this.percentage < 5) {
+                  return null;
+                }
+                return `${this.point.name} ` +
+                  `(${Util.roundStr(this.percentage, 1)}%)`;
+              }
+            }
+          },
+          size: simplified ? '90%' : '80%'
+        }
+      },
+
+      legend: {
+        enabled: false
+      },
+
+      xAxis: {},
+
+      yAxis: {
+        title: { text: yAxis || Text.ChartPercentage }
+      },
+
+      tooltip: Charting.countPointTooltip,
+
+      series: [series]
+    }} />;
+  }
+}
