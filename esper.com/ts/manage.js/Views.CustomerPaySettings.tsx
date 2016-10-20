3ce3481c762eb836c43be0367e10f7b88d1f1ff1
Andrew F <andrewfong@esper.com>
2016-10-20 20:25:12 +0000

    Enterprise billing page

--- esper.com/ts/manage.js/Views.CustomerPaySettings.tsx
+++ esper.com/ts/manage.js/Views.CustomerPaySettings.tsx
@@ -8,11 +8,23 @@ module Esper.Views {
   export class CustomerPaySettings extends CustomerSettings {
     pathFn = Paths.Manage.Customer.pay;
 
-    // TODO
     renderMain(cust: ApiT.Customer) {
-      return <div>
-        Pay
-      </div>;
+      var busy = Stores.Subscriptions.status(cust.id).match({
+        none: () => false,
+        some: (d) => d === Model2.DataStatus.FETCHING
+      });
+      if (busy) {
+        return <div className="esper-spinner" />;
+      }
+
+      let details = Stores.Subscriptions.require(cust.id);
+      return <div className="panel panel-default"><div className="panel-body">
+        <div className="alert alert-info">
+          Your organization is subscribed to
+          the {Text.getPlanName(details.plan)}
+        </div>
+        <Components.CreditCardList subscription={details} />
+      </div></div>;
     }
   }
 }
