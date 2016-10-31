bbedba289684580f19412ba09c22db4d93d52db5
Andrew F <andrewfong@esper.com>
2016-10-31 17:42:47 +0000

    Don't fetch customer data if sandbox user

--- esper.com/ts/time.js/Actions.PaymentInfo.tsx
+++ esper.com/ts/time.js/Actions.PaymentInfo.tsx
@@ -8,6 +8,11 @@ module Esper.Actions {
   export function renderPaymentInfo({teamId}: {
     teamId: string;
   }) {
+    if (Login.data.is_sandbox_user) {
+      Route.nav.home();
+      return;
+    }
+
     let team = Stores.Teams.require(teamId);
     Stores.Subscriptions.fetch(team.team_api.team_subscription.cusid);
     Stores.Customers.refresh();
