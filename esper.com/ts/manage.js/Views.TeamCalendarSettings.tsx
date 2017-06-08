e5f1c22648f857a736f486c30b2e93a480029a74
Andrew Fong <id@andrewfong.com>
2017-06-08 00:10:50 -0700

    Remove plan-based calendar editing constraint
    We can re-implement later but it's currently not enforced elsewhere
    in product (i.e. in lutra-redux), so it's just annoying when
    presented here.

--- esper.com/ts/manage.js/Views.TeamCalendarSettings.tsx
+++ esper.com/ts/manage.js/Views.TeamCalendarSettings.tsx
@@ -5,22 +5,8 @@
 /// <reference path="./Views.TeamSettings.tsx" />
 
 module Esper.Views {
-  interface State {
-    isPlanLimited: boolean;
-  }
-
-  export class TeamCalendarSettings extends TeamSettings<State> {
+  export class TeamCalendarSettings extends TeamSettings<{}> {
     pathFn = Paths.Manage.Team.calendars;
-
-    componentDidUpdate() {
-      this.state = {
-        isPlanLimited: false
-      };
-    }
-
-    displayPlanLimitedMessage() {
-      this.mutateState((s) => s.isPlanLimited = true);
-    }
 
     renderMain(team: ApiT.Team) {
       return <div className="panel panel-default">
@@ -30,13 +16,6 @@ module Esper.Views {
               Text.CalendarSettingsSelfDescription :
               Text.CalendarSettingsExecDescription }
           </div>
-          { this.state.isPlanLimited ?
-            <div className="alert alert-warning">
-              { Text.CalendarLimitMsg(Config.getCalendarLimit(
-                  team.team_api.team_subscription.plan)) }
-            </div>
-            : null
-          }
 
           { Stores.TeamPreferences.get(team.teamid).mapOr(
               <div className="esper-spinner" />,
@@ -61,8 +40,6 @@ module Esper.Views {
       ).mapOr(<div className="esper-spinner" />, ({available, selected}) =>
         <Components.CalendarList
           team={team}
-          onCalendarLimit={this.displayPlanLimitedMessage.bind(this)}
-          limit={Config.getCalendarLimit(team.team_api.team_subscription.plan)}
           availableCalendars={available}
           selectedCalendars={selected}
           listClasses="list-group"
