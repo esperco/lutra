a37495cb00031bea107ebd4d8ac9af503cc7411a
Andrew Fong <id@andrewfong.com>
2017-08-28 22:12:12 -0700

    Disable payment checks

--- ts/time.js/Config.ts
+++ ts/time.js/Config.ts
@@ -13,16 +13,17 @@ module Esper.Config {
 
   /* Config for period selectors */
 
+  // Allow all dates (shutdown)
   export function getMinDate(planid: ApiT.PlanId): Date {
-    if (!planid) return moment()
-      .subtract(2, 'month')
-      .startOf('month')
-      .toDate();
+    // if (!planid) return moment()
+    //   .subtract(2, 'month')
+    //   .startOf('month')
+    //   .toDate();
     switch (planid) {
       case "Basic_20161019":
-        return moment().subtract(2, 'month')
-                       .startOf('month')
-                       .toDate();
+        // return moment().subtract(2, 'month')
+        //                .startOf('month')
+        //                .toDate();
       case "Executive_20161019":
       case "Enterprise_20160923":
       case "Employee_20150304":
@@ -39,6 +40,7 @@ module Esper.Config {
 
   // Config for disabling features for users subscribed to a limited plan
   export function disableAdvancedFeatures(planid: ApiT.PlanId): boolean {
-    return planid === "Basic_20161019";
+    // return planid === "Basic_20161019"; // Enable all features (shutdown)
+    return false;
   }
 }
